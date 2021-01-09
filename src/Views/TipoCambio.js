8a8c51654d01502324e30372f0c1884c38749f24
Alonzo Kuang <akuang@certuit.com>
2021-01-08 16:38:32 -0800

    Nueva vista para Tipo de Cambio.

---
+++ src/Views/TipoCambio.js
@@ -0,0 +1,333 @@
+import React, { useEffect, useState, useMemo } from "react";
+import axios from "axios";
+import Cabecera from "../Components/Template/Cabecera";
+import BarraLateralIzquierda from "../Components/Template/BarraLateralIzquierda";
+import BarraLateralDerecha from "../Components/Template/BarraLateralDerecha";
+import { useTable, useFilters, useGlobalFilter, useSortBy } from 'react-table'
+
+function TipoCambio() {
+
+  const [data, setData] = React.useState([])
+
+  const [state, setState] = React.useState({
+    idTipoCambio: 0,
+    fecha: "",
+    tipoCambio: "",
+    agregar: "Agregar",
+  })
+
+  const handleAceptar = (e) => {
+    e.preventDefault()
+    var params = {
+
+      "m_nIdTipoCambio": state.idTipoCambio,
+      "m_dtFecha": state.fecha,
+      "m_cTipoCambio": state.tipoCambio,
+      "CreadoPor": 1,
+      "ModificadoPor": 1
+    }
+    console.log(params)
+    if (state.idTipoCambio != 0) {
+      const url = `${process.env.REACT_APP_API_URL}/TipoCambio/Modificar/` + state.idTipoCambio;
+      axios.put(url, Object.assign({}, params), { headers }).then(respuesta => {
+        alert(respuesta.data)
+        window.location.reload();
+      }).catch(err => {
+        console.log(err)
+        alert("err")
+      });
+    } else {
+      const url = `${process.env.REACT_APP_API_URL}/TipoCambio/Agregar`;
+      axios.post(url, Object.assign({}, params), { headers }).then(respuesta => {
+        alert(respuesta.data)
+        window.location.reload();
+      }).catch(err => {
+        console.log(err)
+        alert(err)
+      });
+    }
+
+  }
+
+  function handleEliminar(id) {
+    const url = `${process.env.REACT_APP_API_URL}/TipoCambio/Eliminar/` + id;
+    axios.delete(url, { headers }).then(respuesta => {
+      alert(respuesta)
+      window.location.reload();
+    }).catch(err => {
+      alert(err)
+    });
+  }
+
+  function handleShowModificar(id) {
+    const url = `${process.env.REACT_APP_API_URL}/TipoCambio/GetById/${id}`;
+    axios.get(url, { headers }).then(respuesta => {
+      console.log(respuesta.data)
+      setState({
+        ...state,
+        agregar: "Modificar",
+        showPopUp: true,
+        idTipoCambio: id,
+        fecha: respuesta.data.m_dtFecha,
+        tipoCambio: respuesta.data.m_cTipoCambio,
+      })
+    });
+  }
+
+  function handleShowAgregar() {
+    setState({
+      ...state,
+      idTipoCambio: 0,
+      fecha: "",
+      tipoCambio: "",
+      agregar: "Agregar",
+    })
+  }
+
+  const handleChange = event => {
+    console.log(event.target.value)
+    setState({
+      ...state,
+      [event.target.id]: event.target.value
+    });
+  };
+
+  const columns = React.useMemo(() => [
+    {
+      Name: "Fecha",
+      accessor: "m_dtFecha",
+    }, {
+      Name: "Tipo de Cambio",
+      accessor: "m_cTipoCambio",
+    }
+
+  ]);
+
+  useEffect(value => {
+    getAllData();
+  }, []);
+
+  function getAllData() {
+    const url = `${process.env.REACT_APP_API_URL}/TipoCambio/GetListado`;
+    axios.get(url, { headers }).then(respuesta => {
+      setData(respuesta.data)
+    });
+  };
+
+  const headers = {
+    'Content-Type': 'application/json',
+    //    'access-control-allow-origin': '*'
+  }
+
+  function DefaultColumnFilter({
+    column: { filterValue, preFilteredRows, setFilter },
+  }) {
+    const count = preFilteredRows.length
+
+    return (
+      <input
+        className="form-control"
+        value={filterValue || ''}
+        onChange={e => {
+          setFilter(e.target.value || undefined)
+        }}
+        placeholder={`Buscar ${count} registros...`}
+      />
+    )
+  }
+
+  function Table({ columns, data }) {
+
+    const defaultColumn = React.useMemo(
+      () => ({
+        // Default Filter UI
+        Filter: DefaultColumnFilter,
+      }),
+      []
+    )
+
+    const {
+      getTableProps,
+      getTableBodyProps,
+      headerGroups,
+      rows,
+      prepareRow,
+    } = useTable(
+      {
+        columns,
+        data,
+        defaultColumn
+      },
+      useFilters,
+      useGlobalFilter,
+      useSortBy
+    )
+
+    return (
+      <div className="col-md-12">
+        <table className="table" {...getTableProps()}>
+          <thead>
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                <th></th>
+                {headerGroup.headers.map(column => (
+                  // Add the sorting props to control sorting. For this example
+                  // we can add them into the header props
+                  <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                    {column.render('Name')}
+                    {/* Add a sort direction indicator */}
+                    <span>
+                      {column.isSorted
+                        ? column.isSortedDesc
+                          ? <i className="fa fa-caret-up" />
+                          : <i className="fa fa-caret-down" />
+                        : ''}
+                    </span>
+                    <div>{column.canFilter ? column.render('Filter') : null}</div>
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map(
+              (row, i) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()}>
+                    <td>
+                      <div>
+                        <a href="#Agregar" role="tab" data-toggle="tab" onClick={() => (handleShowModificar(row.original.m_nIdTipoCambio))} className="btn btn-default btn-sm m-user-edit"><i className="zmdi zmdi-edit" /></a>
+                        <a href="#" className="btn btn-default btn-sm m-user-delete" onClick={() => (handleEliminar(row.original.m_nIdTipoCambio))}><i className="zmdi zmdi-close" /></a>
+                      </div>
+                    </td>
+                    {row.cells.map(cell => {
+                      return (
+                        <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                      )
+                    })}
+                  </tr>
+                )
+              }
+            )}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+
+      <header className="topbar clearfix">
+        <Cabecera />
+      </header>
+
+      {/*Leftbar Start Here*/}
+      <aside className="iconic-leftbar">
+        <BarraLateralIzquierda />
+      </aside>
+      {/*Leftbar End Here*/}
+
+      {/*Page Container Start Here*/}
+      <section className="main-container">
+        <div className="container-fluid">
+
+          <div className="page-header full-block light">
+            <h2>Tipo de Cambio</h2>
+          </div>
+
+          <ul className="nav nav-tabs">
+            <li className="active">
+              <a data-toggle="tab" href="#Listado">
+                <i className="fa fa-list" /> Listado
+            </a>
+            </li>
+            <li>
+              <a data-toggle="tab" href="#Agregar" onClick={handleShowAgregar}>
+                <i className="fa fa-plus-circle" /> {state.agregar}
+              </a>
+            </li>
+          </ul>
+
+          <div className="row" className="tab-content">
+            <div className="widget-wrap" id="Listado" className="tab-pane fade in active">
+              <div className="widget-wrap">
+                <div className="widget-content">
+                  <div className="row">
+                    <Table columns={columns} data={data} />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="widget-wrap" id="Agregar" className="tab-pane fade">
+              <div className="widget-wrap">
+                <div className="widget-content">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <form className="j-forms" onSubmit={handleAceptar}>
+                        <div className="form-content">
+
+                          <div className="col-sm-12 col-md-12 unit">
+                            <label className="label">
+                              Fecha
+                          </label>
+                            <div className="input">
+                              <input
+                                onChange={handleChange}
+                                className="form-control"
+                                type="datetime-local"
+                                required={true}
+                                value={state.fecha}
+                                id="fecha"
+                              />
+                            </div>
+                          </div>
+
+                          <div className="col-sm-12 col-md-12 unit">
+                            <label className="label">
+                              Tipo de Cambio
+                          </label>
+                            <div className="input">
+                              <input
+                                onChange={handleChange}
+                                className="form-control"
+                                type="number"
+                                required={true}
+                                value={state.tipoCambio}
+                                id="tipoCambio"
+                              />
+                            </div>
+                          </div>
+
+                        </div>
+                        <br></br>
+                        <div className="form-footer" className="col-md-12">
+                          <button href="#Listado" role="tab" data-toggle="tab" className="btn btn-primary secondary-btn">Cancelar</button>
+                          <button type="submit" className="btn btn-primary primary-btn">Aceptar</button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </section>
+      {/*Page Container End Here*/}
+
+      {/*Rightbar Start Here*/}
+      <aside className="rightbar">
+        <BarraLateralDerecha />
+      </aside>
+
+    </div>
+
+  );
+}
+
+export default TipoCambio;
