3ba0dd618c5b2104933788bb5103a6ac0d8e3ec7
akuang <akuang@certuit.com>
2021-03-01 10:20:48 -0800

    Se agregó diseño a Login

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -62,141 +62,73 @@ function Login() {
   return (
     <section className="login-container" >
       <div className="split left">
-        <div className="col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4">
-          <div className="login-form-container">
-            <div className="row">
 
-              <div className="login-form-header">
-                <div className="logo">
-                  <a href="index.html" title="Admin Template"><img src="iconos/GM-naranja.png" width="230" height="58" alt="logo" /></a>
-                </div>
-              </div>
-              <div className="login-form-content">
-                {/* start login */}
-
-                <div className="unit">
-                  <div className="input login-input">
-                    <label className="icon-left" htmlFor="login">
-                      <i className="zmdi zmdi-account" />
-                    </label>
-                    <input className="form-control login-frm-input" type="text" id="rfc" name="rfc" placeholder="RFC" />
-                  </div>
-                </div>
-                <div className="unit">
-                  <div className="input login-input">
-                    <label className="icon-left" htmlFor="login">
-                      <i className="zmdi zmdi-account" />
-                    </label>
-                    <input className="form-control login-frm-input" type="text" id="usuario" name="usuario" placeholder="Usuario" />
-                  </div>
-                </div>
-                {/* end login */}
-                {/* start password */}
-                <div className="unit">
-                  <div className="input login-input">
-                    <label className="icon-left" htmlFor="Contraseña">
-                      <i className="zmdi zmdi-key" />
-                    </label>
-                    <input className="form-control login-frm-input" type="password" id="password" name="password" placeholder="Password" />
-                    <span className="hint">
-                      <a href="#" className="link">Olvidaste la contraseña?</a>
-                    </span>
-                  </div>
-                </div>
-                {/* end password */}
-                {/* start keep logged */}
-
-                {/* end keep logged */}
-                {/* start response from server */}
-                <div className="response" />
-                {/* end response from server */}
-              </div>
-              <div className="login-form-footer">
-                <button onClick={() => login()} className="btn-block btn btn-primary">Inicia Sesión</button>
-              </div>
-
-            </div>
-          </div>
-        </div>
       </div>
 
       <div className="split right">
-        <div className="login-form-container">
-          <div className="row">
+        <div className="centered">
+          {/* start login */}
 
-            <div className="login-form-header">
-              <div className="logo">
-                <a href="index.html" title="Admin Template"><img src="iconos/GM-naranja.png" width="230" height="58" alt="logo" /></a>
-              </div>
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 unit">
+            <div className="input login-input">
+              <label className="icon-left" htmlFor="rfc">
+                <i className="zmdi zmdi-account" />
+              </label>
+              <input className="form-control login-frm-input" type="text" id="rfc" name="rfc" placeholder="RFC" />
             </div>
-
-            <div className="login-form-content centered">
-              {/* start login */}
-
-              <div className="col-xs-11 col-sm-11 col-md-11 col-lg-11 unit">
-                <div className="input login-input">
-                  <label className="icon-left" htmlFor="login">
-                    <i className="zmdi zmdi-account" />
-                  </label>
-                  <input className="form-control login-frm-input" type="text" id="rfc" name="rfc" placeholder="RFC" />
-                </div>
-              </div>
-
-              <div className="col-xs-11 col-sm-11 col-md-11 col-lg-11 unit">
-                <div className="input login-input">
-                  <label className="icon-left" htmlFor="login">
-                    <i className="zmdi zmdi-account" />
-                  </label>
-                  <input className="form-control login-frm-input" type="text" id="usuario" name="usuario" placeholder="Usuario" />
-                </div>
-              </div>
-              {/* end login */}
-
-              {/* start password */}
-              <div className="col-xs-11 col-sm-11 col-md-11 col-lg-11 unit">
-                <div className="input login-input">
-                  <label className="icon-left" htmlFor="Contraseña">
-                    <i className="zmdi zmdi-key" />
-                  </label>
-                  <input className="form-control login-frm-input" type="password" id="password" name="password" placeholder="Password" />
-                  <span className="hint">
-                    <a href="#" className="link">Olvidaste la contraseña?</a>
-                  </span>
-                </div>
-              </div>
-              {/* end password */}
-
-              <div className="col-xs-11 col-sm-11 col-md-11 col-lg-11">
-                <button onClick={() => login()} className="btn-block btn btn-primary">Inicia Sesión</button>
-              </div>
-
-            </div>
-
           </div>
 
-          <div className="login-form-footer" style={{}}>
-            <button className="buttonCircle">
-              <a href="http://www.facebook.com/GMTransportOficial/" target="_blank">
-                <i className="fa fa-facebook" />
-              </a>
-            </button>
-            <button className="buttonCircle">
-              <a href=" https://www.instagram.com/grupogmtransport" target="_blank">
-                <i className="fa fa-instagram" />
-              </a>
-            </button>
-            <button className="buttonCircle">
-              <a href="http://bit.ly/paqueteriaGM" target="_blank">
-                <i className="fa fa-whatsapp" />
-              </a>
-            </button>
-            <button className="buttonCircle">
-              <a href="https://twitter.com/gmtransporterp?lang=es" target="_blank">
-                <i className="fa fa-twitter" />
-              </a>
-            </button>
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 unit">
+            <div className="input login-input">
+              <label className="icon-left" htmlFor="usuario">
+                <i className="zmdi zmdi-account" />
+              </label>
+              <input className="form-control login-frm-input" type="text" id="usuario" name="usuario" placeholder="Usuario" />
+            </div>
           </div>
+          {/* end login */}
 
+          {/* start password */}
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 unit">
+            <div className="input login-input">
+              <label className="icon-left" htmlFor="password">
+                <i className="zmdi zmdi-key" />
+              </label>
+              <input className="form-control login-frm-input" type="password" id="password" name="password" placeholder="Contraseña" />
+              <span className="hint">
+                <a href="#" className="link" style={{ color: "#FFFFFF" }}>Olvidaste la contraseña?</a>
+              </span>
+            </div>
+          </div>
+          {/* end password */}
+
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <button onClick={() => login()} className="btn-block btn btn-primary">Inicia Sesión</button>
+          </div>
+          <div style={{textAlign: "center"}}>
+            <ul className="social-media">
+              <li className="social-media-item">
+                <a href="http://www.facebook.com/GMTransportOficial/" target="_blank">
+                  <i className="fa fa-facebook circle-icon" />
+                </a>
+              </li>
+              <li className="social-media-item">
+                <a href=" https://www.instagram.com/grupogmtransport" target="_blank">
+                  <i className="fa fa-instagram circle-icon" />
+                </a>
+              </li>
+              <li className="social-media-item">
+                <a href="http://bit.ly/paqueteriaGM" target="_blank">
+                  <i className="fa fa-whatsapp circle-icon" />
+                </a>
+              </li>
+              <li className="social-media-item">
+                <a href="https://twitter.com/gmtransporterp?lang=es" target="_blank">
+                  <i className="fa fa-twitter circle-icon" />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
 
