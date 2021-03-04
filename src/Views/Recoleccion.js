0f40b4747139f7e2b0feb6dca90e646901b502f1
akuang <akuang@certuit.com>
2021-03-04 11:15:56 -0800

    Se empezó a hacer generar embarque

--- src/Views/Recoleccion.js
+++ src/Views/Recoleccion.js
@@ -2181,6 +2181,12 @@ function Recoleccion() {
               </a>
             </li>
 
+            <li style={{float: "right"}}>
+              <a href="/Embarque/idRecoleccion=21">
+                Generar Embarque
+              </a>
+            </li>
+
             {/**<button className="topbar-right pull-right">Boton</button>*/}
           </ul>
 
