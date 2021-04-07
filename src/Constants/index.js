8b4333200582c80ee16dd7c955387389b0a46bf7
akuang <akuang@certuit.com>
2021-04-07 10:14:13 -0700

    Se agregó la función de imprimir en clientes.

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -9,24 +9,24 @@ export const API_VERSION = 'v1.0.0';
 export const APP_TITLE = "Sistemas Sierra";
 export const OAUTH2_REDIRECT_URI = 'http://192.168.1.185:8080/sierra/oauth2/redirect';
 export const API_HEADERS = {
-    'Accept': 'application/vnd.certuit-'+ API_VERSION +'+json' ,
+    'Accept': 'application/vnd.certuit-' + API_VERSION + '+json',
     'Content-Type': 'application/json',
 };
 
 export const API_AUTENTICATION_HEADERS = {
-    'Accept': 'application/vnd.certuit-'+ API_VERSION +'+json' ,
+    'Accept': 'application/vnd.certuit-' + API_VERSION + '+json',
     'Content-Type': 'application/json',
-    'Authorization' : 'Bearer ' + localStorage.getItem(ACCESS_TOKEN),
+    'Authorization': 'Bearer ' + localStorage.getItem(ACCESS_TOKEN),
 };
 
 export const API_BASIC_HEADERS = {
-    'Accept': 'application/vnd.certuit-'+ API_VERSION +'+json' ,
-    'Authorization' : 'Bearer ' + localStorage.getItem(ACCESS_TOKEN),
+    'Accept': 'application/vnd.certuit-' + API_VERSION + '+json',
+    'Authorization': 'Bearer ' + localStorage.getItem(ACCESS_TOKEN),
 };
 export const API_MULTIPART_HEADERS = {
-    'Accept': 'application/vnd.certuit-'+ API_VERSION +'+json' ,
+    'Accept': 'application/vnd.certuit-' + API_VERSION + '+json',
     'Content-Type': 'multipart/form-data',
-    'Authorization' : 'Bearer ' + localStorage.getItem(ACCESS_TOKEN),
+    'Authorization': 'Bearer ' + localStorage.getItem(ACCESS_TOKEN),
 };
 
 export function TABLE_OPTIONS(filename, searchOpen = true, onRowClick) {
@@ -42,7 +42,7 @@ export function TABLE_OPTIONS(filename, searchOpen = true, onRowClick) {
         onCellClick: onRowClick,
         print: false,
         expandableRowsOnClick: true,
-        downloadOptions: {filename: filename + '.csv', separator: ','},
+        downloadOptions: { filename: filename + '.csv', separator: ',' },
         // customToolbar: () => {
         //   return <CustomToolbar onClick={addRowAction} />;
         // },
@@ -85,3 +85,76 @@ export function TABLE_OPTIONS(filename, searchOpen = true, onRowClick) {
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const GITHUB_AUTH_URL = API_BASE_URL + '/oauth2/authorize/github?redirect_uri=' + OAUTH2_REDIRECT_URI;
+
+export const dataGridLocaleText = {
+    // Root
+    rootGridLabel: 'grid',
+    noRowsLabel: 'Sin registro',
+    errorOverlayDefaultLabel: 'A ocurrido un error al cargar los datos.',
+
+    // Filters toolbar button text
+    toolbarFilters: 'Filtros',
+    toolbarFiltersLabel: 'Mostrar filtro',
+    toolbarFiltersTooltipHide: 'Ocultar filtro',
+    toolbarFiltersTooltipShow: 'Mostrar filtro',
+    toolbarFiltersTooltipActive: (count) =>
+        count !== 1 ? `${count} active filters` : `${count} active filter`,
+
+    // Export selector toolbar button text
+    toolbarExport: 'Exportar',
+    toolbarExportLabel: 'Exportar',
+    toolbarExportCSV: 'Descargar como CSV',
+
+    // Columns panel text
+    columnsPanelTextFieldLabel: 'Buscar columna',
+    columnsPanelTextFieldPlaceholder: 'Título de la Columna',
+    columnsPanelDragIconLabel: 'Reordenar columna',
+    columnsPanelShowAllButton: 'Mostrar todo',
+    columnsPanelHideAllButton: 'Ocultar todo',
+
+    // Filter panel text
+    filterPanelAddFilter: 'Agregar filtro',
+    filterPanelDeleteIconLabel: 'Eliminar',
+    filterPanelOperators: 'Operador',
+    filterPanelOperatorAnd: 'Y',
+    filterPanelOperatorOr: 'O',
+    filterPanelColumns: 'Columna',
+    filterPanelInputLabel: 'Valor',
+    filterPanelInputPlaceholder: 'Valor de filtrado',
+
+    // Filter operators text
+    filterOperatorContains: 'contiene',
+    filterOperatorEquals: 'igual a',
+    filterOperatorStartsWith: 'empieza con',
+    filterOperatorEndsWith: 'termina con',
+    filterOperatorIs: 'igual a',
+    filterOperatorNot: 'diferente a',
+    filterOperatorAfter: 'después de',
+    filterOperatorOnOrAfter: 'is on or after',
+    filterOperatorBefore: 'is before',
+    filterOperatorOnOrBefore: 'is on or before',
+
+    // Column menu text
+    columnMenuLabel: 'Menú',
+    columnMenuShowColumns: 'Mostrar columna',
+    columnMenuFilter: 'Filtro',
+    columnMenuHideColumn: 'Ocultar columna',
+    columnMenuUnsort: 'Desordenar',
+    columnMenuSortAsc: 'Ascendiente',
+    columnMenuSortDesc: 'Descendiente',
+
+    // Rows selected footer text
+    footerRowSelected: (count) =>
+        count !== 1
+            ? `${count.toLocaleString()} renglones seleccionados`
+            : `${count.toLocaleString()} renglón seleccionado`,
+
+    // Total rows footer text
+    footerTotalRows: 'Renglones totales:',
+
+    backIconButtonText: 'Página anterior',
+    labelRowsPerPage: 'Filas por página:',
+    labelDisplayedRows: ({ from, to, count }) =>
+      `${from}-${to} de ${count !== -1 ? count : `more than ${to}`}`,
+    nextIconButtonText: 'Siguiente página',
+}
