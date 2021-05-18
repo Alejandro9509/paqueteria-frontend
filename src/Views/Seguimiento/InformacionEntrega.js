cee065b99f269cdf7d0a7d87857e057ac77c37fe
Esteban Valencia <evalencia@certuit.com>
2021-05-17 20:04:16 -0600

    Se hicieron ajustes en el componente Detalles de entrega

--- src/Views/Seguimiento/InformacionEntrega.js
+++ src/Views/Seguimiento/InformacionEntrega.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
-const labelContainerStyle ={
-    margin: '10px'
-}
 
-const labelStyle = {
-    color: '#F9A03E'
-}
-const detailsContainerStyle = {
-    display: 'flex',
-    flexDirection: 'row'
-}
+const useStyles = makeStyles((theme) =>({
+    labelContainer:{
+        margin: 5
+    },
+    label:{
+        color: theme.palette.primary.main
+    },
+    detailsContainer: {
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    h3:{
+        margin: 0
+    }
+}));
 
 export default function InformacionEntrega(props){
     console.log(props.entrega);
+    const classes = useStyles();
     const destinationAddress = props.entrega.destinationAddress;
     const id = props.entrega.id;
     const date = props.entrega.date;
@@ -22,12 +29,12 @@ export default function InformacionEntrega(props){
     return(
         <div>
             <h3>Dirección de entrega</h3>
-            <p>{destinationAddress}</p>
+            <span className={classes.labelContainer}>{destinationAddress}</span>
             <h3>Detalle de Envío</h3>
-            <div style={detailsContainerStyle}>
-                <div style={labelContainerStyle}><span style={labelStyle}>Folio: </span><span>{id}</span></div>
-                <div style={labelContainerStyle}><span style={labelStyle}>Fecha: </span><span>{date}</span></div>
-                <div style={labelContainerStyle}><span style={labelStyle}>Tipo de servicio: </span><span>{serviceType}</span></div>
+            <div className={classes.detailsContainer}>
+                <div className={classes.labelContainer}><span className={classes.label}>Folio: </span><span>{id}</span></div>
+                <div className={classes.labelContainer}><span className={classes.label}>Fecha: </span><span>{date}</span></div>
+                <div className={classes.labelContainer}><span className={classes.label}>Tipo de servicio: </span><span>{serviceType}</span></div>
             </div>
             
         </div>
