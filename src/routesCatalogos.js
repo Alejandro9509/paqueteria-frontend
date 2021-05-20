c552d462ccaf05b0051b4f513f9672b7f31cb246
Esteban Valencia <jmartinez@certuit.com>
2021-05-19 20:07:40 -0600

    Se terminó la pantalla de configuracion para correo de seguimiento.

--- src/routesCatalogos.js
+++ src/routesCatalogos.js
@@ -23,7 +23,7 @@ import EstatusDocumentoPage from './Views/EstatusDocumento';
 import ConceptosFacturacionPage from './Views/ConceptosFacturacion/ConceptosFacturacion';
 import TarifasPage from './Views/Tarifas/Tarifas';
 import ClasificacionViajePage from './Views/ClasificacionViajes/ClasificacionViaje';
-import ConfigSeguimiento from './Views/ConfigSeguimiento';
+import PlantillaCorreo from './Views/PlantillaCorreo';
 
 import {ReactComponent as GClienteIcon} from './iconos/Catalogos/Icono Grupo Clientes/icono_grupo_cliente.svg';
 import {ReactComponent as BancoIcon} from './iconos/Catalogos/Icono Banco/icono_banco.svg';
@@ -225,10 +225,10 @@ const catalogRoutes = [
     component: EmbalajesPage,
   },
   {
-    path: "/ConfigSeguimiento",
-    name: "Configurar Seguimiento",
+    path: "/PlantillaCorreo",
+    name: "Plantilla de Correo",
     icon: <PConfiguracionIcon/>,
-    component: ConfigSeguimiento,
+    component: PlantillaCorreo,
   },
 ];
 
