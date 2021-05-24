97912307c3e4fd71a8ea70b30d90451781028a08
Esteban Valencia <evalencia@certuit.com>
2021-05-24 13:29:26 -0600

    *Se Agregó funcionamiento de mostrar pdf de ayuda. *Se agregaron los botones para shortcuts y menu.

--- src/Components/Template/Cabecera.js
+++ src/Components/Template/Cabecera.js
@@ -1,11 +1,51 @@
 import { Hidden } from "@material-ui/core";
 import React from "react";
+import iconoAyuda from '../../iconos/Cabecera/icono_ayuda.svg';
+import iconoShortcuts from '../../iconos/Cabecera/icono_shortcuts.svg';
+import iconoMenu from '../../iconos/Cabecera/icono_menu.svg';
+import pdf from './prueba.pdf';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function Cabecera({ titulo, children }) {
+
+    const shortcuts =[
+        'Item 1',
+        'Item 2',
+    ];
+    const menu_items = [
+        'Accesos director',
+        'GM Noticias',
+        'Tutoriales',
+        'Ayuda en línea',
+        'Actualizaciónes',
+        'Quejas y sugerencias',
+    ];
+
+    const ITEM_HEIGHT = 48;
 
     function logout() {
         localStorage.removeItem("accessToken");
     }
+    const [shortcutsVisible, setShortcutsVisible] = React.useState(null);
+    const [menuVisible, setMenuVisible] = React.useState(null);
+    const menuOpen = Boolean(menuVisible);
+    const shortcutsOpen = Boolean(shortcutsVisible);
+
+    const handleShortcutsClick = (event) => {
+        setShortcutsVisible(event.currentTarget);
+    };
+    const handleShortcutsClose = () => {
+        setShortcutsVisible(null);
+    };
+    const handleMenuClick = (event) => {
+        setMenuVisible(event.currentTarget);
+    };
+    const handleMenuClose =()=> {
+        setMenuVisible(null);
+    };
 
     return (
         <div>
@@ -32,11 +72,16 @@ function Cabecera({ titulo, children }) {
                 </div>
             </Hidden>
 
-            <div className="topbar-right pull-right iconic-aside-container">
-                <div className="clearfix">
-                    <div className="user-profile-container">
+            <div className="topbar-right pull-right iconic-aside-container" style={{display:'flex', flexDirection:'row', alignItems: 'center', height: 60}}>
+                <div>
+                    <a href={pdf} target={"_blank"}>
+                        <img src={iconoAyuda} style={{height: 30, width:30, margin: 10}}/>
+                    </a>
+                </div>
+                <div className="clearfix" style={{display:'block'}}>
+                    <div className="user-profile-container" style={{height: 40}}>
                         <div className="user-profile clearfix">
-                            <div className="admin-user-thumb">
+                            <div className="admin-user-thumb" style={{padding: '0px 0px 0px 0px'}}>
                                 <img src="images/avatar/jaman_01.jpg" alt="admin" />
                             </div>
                             <div className="admin-user-info">
@@ -60,6 +105,97 @@ function Cabecera({ titulo, children }) {
                             </ul>
                         </div>
                     </div>
+                </div>
+                <div style={{position: "relative"}}>
+                    <IconButton
+                        aria-label="shortcuts"
+                        aria-controls="shortcuts-menu"
+                        aria-haspopup="true"
+                        onClick={handleShortcutsClick}>
+                        <img src={iconoShortcuts} style={{height:30, width:30, margin: 10}}/>
+                    </IconButton>
+                    <Menu
+                        id="shortcuts-menu"
+                        anchorEl={shortcutsVisible}
+                        keepMounted
+                        open={shortcutsOpen}
+                        onClose={handleShortcutsClose}
+                        PaperProps={{
+                            style:{
+                                maxHeight: ITEM_HEIGHT * 4.5,
+                                width: '20ch',
+                            },
+                        }}>
+                        {shortcuts.map((option) => (
+                            /*<a key={option} onClick={handleShortcutsClose} href={'#'}>
+                                <img src={iconoShortcuts} style={{height:20, width:20, margin:10}}/>
+                                <p>{option}</p>
+                            </a>*/
+                            <MenuItem key={option} onClick={handleShortcutsClose}>
+                                {option}
+                            </MenuItem>
+                        ))}
+                    </Menu>
+                    {/*<img src={iconoShortcuts} style={{height:30, width:30, margin: 10}}/>
+                    <div className="shortcuts-menu"
+                         style={{
+                             backgroundColor:'#FFFF',
+                             display:"none",
+                             gridTemplateColumns: 'auto auto auto',
+                             position: "absolute"}}>
+                        <a href={'#'}>
+                            <img src={iconoShortcuts} style={{height:20, width:20, margin:10}}/>
+                            <p>Shortcut 1</p>
+                        </a>
+                        <a href={'#'}>
+                            <img src={iconoShortcuts} style={{height:20, width:20, margin:10}}/>
+                            <p>Shortcut 2</p>
+                        </a>
+                        <a href={'#'}>
+                            <img src={iconoShortcuts} style={{height:20, width:20, margin:10}}/>
+                            <p>Shortcut 3</p>
+                        </a>
+                        <a href={'#'}>
+                            <img src={iconoShortcuts} style={{height:20, width:20, margin:10, display:"block"}}/>
+                            <p>Shortcut 4</p>
+                        </a>
+                    </div>*/}
+                </div>
+                <div>
+                    <IconButton
+                        aria-label="more"
+                        aria-controls="menu"
+                        aria-haspopup="true"
+                        onClick={handleMenuClick}>
+                        <img src={iconoMenu} style={{height:30, width:30, margin: 10}}/>
+                    </IconButton>
+                    <Menu
+                        id="menu"
+                        anchorEl={menuVisible}
+                        keepMounted
+                        open={menuOpen}
+                        onClose={handleMenuClose}
+                        PaperProps={{
+                            style:{
+                                maxHeight: ITEM_HEIGHT * 4.5,
+                                width: '20ch',
+                            },
+                        }}>
+                        {menu_items.map((option) => (
+                            <MenuItem key={option} onClick={handleMenuClose}>
+                                {option}
+                            </MenuItem>
+                        ))}
+                    </Menu>
+                    {/*<img src={iconoMenu} style={{height:30, width:30, margin: 10}}/>
+                    <div className="menu-items" style={{display:"none", position: "absolute", backgroundColor:'#FFFF',}}>
+                        <a href="#">Accesos director</a>
+                        <a href="#">GM Noticias</a>
+                        <a href={'#'}>Tutoriales</a>
+                        <a href={'#'}>Ayuda en línea</a>
+                        <a href={'#'}>Actualizaciónes</a>
+                        <a href={'#'}>Quejas y sugerencias</a>
+                    </div>*/}
                 </div>
             </div>
             <Hidden smDown implementation="css">
