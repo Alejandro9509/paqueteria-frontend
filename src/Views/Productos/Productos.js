a92fa79f3c00ef337ebfd17cd148be98f7886132
Esteban Valencia <evalencia@certuit.com>
2021-06-22 11:52:12 -0600

    *Se agregó catálogo para Productos

--- src/Views/Productos/Productos.js
+++ src/Views/Productos/Productos.js
@@ -2,42 +2,26 @@ import React from 'react';
 import Cabecera from "../../Components/Template/Cabecera";
 import BarraLateralIzquierda from "../../Components/Template/BarraLateralIzquierda";
 import {DataGrid} from "@material-ui/data-grid";
-import {dataGridLocaleText} from "../../Constants";
-import {FormControl, InputLabel, MenuItem, Select, TextField, Tooltip} from "@material-ui/core";
+import {FormControl, InputLabel, Select, TextField, Tooltip} from "@material-ui/core";
 import {confirmAlert} from "react-confirm-alert";
-import {makeStyles} from "@material-ui/core/styles";
+import {dataGridLocaleText} from "../../Constants";
 
-const useStyles = makeStyles((theme) => ({
-    formControl: {
-        margin: theme.spacing(1),
-        minWidth: 120,
-    },
-    selectEmpty: {
-        marginTop: theme.spacing(2),
-    },
-}));
-
-function CondicionesRecepcionEntrega(){
-    const classes = useStyles();
+function Productos(){
 
     const [state, setState] = React.useState({
         agregar: "Agregar",
-        height: window.innerHeight,
-        descripcion: "",
-        activo: 0,
-    })
+        height: window. innerHeight,
+        descripcion : "",
+    });
+
     const [data, setData] = React.useState([
         {
             id: 0,
-            descripcion: "Descripcion uno",
-            activo: 1,
-            activonName: "Sí"
+            descripcion: "Descripción uno",
         },
         {
             id: 1,
-            descripcion: "Descripcion dos",
-            activo: 0,
-            activonName: "No"
+            descripcion: "Desctipcion Dos",
         }
     ])
     const columns = React.useMemo(() => [
@@ -71,18 +55,18 @@ function CondicionesRecepcionEntrega(){
                             <a href="#"
                                className="btn btn-default btn-xs"
                                onClick={() => confirmAlert({
-                                title: 'Confirmar Eliminar',
-                                message: 'Está seguro de eliminar Condición?',
-                                buttons: [
-                                    {
-                                        label: 'Si',
-                                        onClick: () => handleEliminar(row.row)
-                                    },
-                                    {
-                                        label: 'No',
-                                    }
-                                ]
-                            })}>
+                                   title: 'Confirmar Eliminar',
+                                   message: 'Está seguro de eliminar Condición?',
+                                   buttons: [
+                                       {
+                                           label: 'Si',
+                                           onClick: () => handleEliminar(row.row)
+                                       },
+                                       {
+                                           label: 'No',
+                                       }
+                                   ]
+                               })}>
                                 <i className="zmdi zmdi-delete" style={{ color: "#F30B0B" }} />
                             </a>
                         </Tooltip>
@@ -100,26 +84,20 @@ function CondicionesRecepcionEntrega(){
             field: "descripcion",
             width: 400,
         },
-        {
-            headerName: "Activo",
-            field: "activonName",
-            width: 200,
-        }
     ]);
 
     function handleShowAgregar(){
-        setState({
-            ...state,
-            agregar: "Agregar",
-        })
+     setState({
+         ...state,
+         agregar: "Agregar",
+     });
     }
 
     function handleShowModificar(row){
-        console.log(row.id)
+        console.log(row)
         setState({
             ...state,
             descripcion: row.descripcion,
-            activo: row.activo
         })
     }
 
@@ -127,20 +105,16 @@ function CondicionesRecepcionEntrega(){
         console.log(row)
     }
 
-    const handleChange = (event) =>{
-        console.log("change")
-    };
-
-    const handleAceptar = (event) => {
+    const handleAceptar = (e) => {
         console.log("submit")
     };
-
-    const handleChangeActive = (event) =>{
+    const handleChange = (e) => {
         setState({
             ...state,
-            activo: event.target.value
+            [e.target.id]: e.target.value
         });
-    };
+    }
+
     return(
         <div>
             <header className="topbar clearfix">
@@ -163,10 +137,9 @@ function CondicionesRecepcionEntrega(){
             </aside>
             {/*Leftbar End Here*/}
             <section className={"main-container"}>
-                <div className={"container-fluid"}>
-                    {/** TABS SUPERIORES*/}
+                <div className={"content-fluid"}>
                     <ul className={"nav navStatica nav-tabs"}>
-                        <li className={'active'}>
+                        <li className={"active"}>
                             <a data-toggle={"tab"} href={"#Listado"}>
                                 <i className={"fa fa-list"}/> Listado
                             </a>
@@ -177,24 +150,20 @@ function CondicionesRecepcionEntrega(){
                             </a>
                         </li>
                     </ul>
-                    {/** TABLAS*/}
+
                     <div className={"row"} className={"tab-content"}>
                         <div className="widget-wrap" id="Listado" className="tab-pane fade in active">
                             <div className="widget-wrap">
                                 <div className="widget-content">
-                                    <div className="row" style={{ height: state.height - 250, width: '100%' }}>
-                                        <DataGrid
-                                            columns={columns}
-                                            rows={data}
-                                            localeText={dataGridLocaleText}
-                                            density={"compact"}
-                                            pageSize={Math.floor((state.height -310) / 30)}
-                                            />
+                                    <div className={"row"} style={{height: state.height -250, width: '100%'}}>
+                                        <DataGrid columns={columns} rows={data}
+                                                  locateText={dataGridLocaleText}
+                                                  density={"compact"}
+                                                  pageSize={Math.floor((state.height - 310) / 30)}/>
                                     </div>
                                 </div>
                             </div>
                         </div>
-
                         <div className="widget-wrap" id="Agregar" className="tab-pane fade">
                             <div className="widget-wrap">
                                 <div className="widget-content">
@@ -217,25 +186,6 @@ function CondicionesRecepcionEntrega(){
                                                                            maxLength="50"/>
                                                             </div>
                                                         </div>
-                                                        <div className="col-sm-6 col-md-2-5 unit">
-                                                            <label className="input select">
-                                                                <FormControl fullWidth variant="outlined" margin="dense">
-                                                                    <InputLabel id="idActivoLabel">Activo</InputLabel>
-                                                                    <Select
-                                                                        label="Activo"
-                                                                        labelId="idActivo"
-                                                                        onChange={handleChangeActive}
-                                                                        className="form-control"
-                                                                        value={state.activo}
-                                                                        required
-                                                                        name="activo"
-                                                                        id="activo">
-                                                                        <option value={0}>No</option>
-                                                                        <option value={1}>Sí</option>
-                                                                    </Select>
-                                                                </FormControl>
-                                                            </label>
-                                                        </div>
                                                     </div>
                                                     <div className="row">
                                                         <div className="form-footer" className="col-sm-6 col-md-5 unit">
@@ -248,11 +198,8 @@ function CondicionesRecepcionEntrega(){
                                                             <button type="submit"
                                                                     className="btn btn-primary primary-btn">Aceptar</button>
                                                         </div>
-
                                                     </div>
                                                 </div>
-
-
                                             </form>
                                         </div>
                                     </div>
@@ -260,11 +207,10 @@ function CondicionesRecepcionEntrega(){
                             </div>
                         </div>
                     </div>
-
                 </div>
             </section>
         </div>
     )
 }
 
-export default CondicionesRecepcionEntrega;
+export default Productos;
--- src/routesCatalogos.js
+++ src/routesCatalogos.js
@@ -26,6 +26,7 @@ import ParametrosConfiguracionPage from './Views/ParametrosConfiguracion/Paramet
 import ClasificacionViajePage from './Views/ClasificacionViajes/ClasificacionViaje';
 import PlantillaCorreo from './Views/PlantillaCorreo';
 import CondicionesRecepcionEntrega from './Views/CondicionesRecepcionEntrega/CondicionesRecepcionEntrega';
+import Productos from "./Views/Productos/Productos";
 
 import {ReactComponent as TServicioIcon} from './iconos/Catalogos/Icono Tipos Servicio/icono_tipo_servicio.svg';
 
@@ -241,6 +242,12 @@ const catalogRoutes = [
     name: "Condiciones de recepcion y entrega",
     icon: <PConfiguracionIcon/>,
     component: CondicionesRecepcionEntrega,
+  },
+  {
+    path: "/Productos",
+    name: "Productos",
+    icon: <PConfiguracionIcon/>,
+    component: Productos,
   },
 ];
 
