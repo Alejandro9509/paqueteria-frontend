6c4f729980dc830af7c595a7462a553da41937f8
akuang <akuang@certuit.com>
2021-07-23 17:29:51 -0700

    Se arregló el bug de modificar.

--- src/Views/Tarifas/ConceptosAdicionalesRecoleccion.js
+++ src/Views/Tarifas/ConceptosAdicionalesRecoleccion.js
@@ -80,7 +80,7 @@ class ConceptosAdicionales extends Component {
                     this.props.removeConcepto(i)
                 })
                 this.props.select.m_arrArConceptos.forEach(element => {
-                    this.props.addConcepto({ concepto: respuesta.data.find(c => c.m_nIdConceptosFacturacion === element.m_nIdConceptoFacturacion), importe: element.m_cImporte, traslada: element.m_nIdImpuestoTraslada, importeIVA: element.m_cImporteIva, retiene: element.m_nIdImpuestoRetiene, importeRet: element.m_cImporteRetiene, nombreConcepto: element.m_sConcepto, rangoMinimo: element.m_xnRangoMinimo, rangoMaximo: element.m_xnRangoMaximo })
+                    this.props.addConcepto({ concepto: respuesta.data.find(c => c.m_nIdConceptosFacturacion === element.m_nIdConceptosFacturacion), importe: element.m_cImporte, traslada: element.m_nIdImpuestoTraslada, importeIVA: element.m_cImporteIva, retiene: element.m_nIdImpuestoRetiene, importeRet: element.m_cImporteRetiene, nombreConcepto: element.m_sConcepto, rangoMinimo: element.m_xnRangoMinimo, rangoMaximo: element.m_xnRangoMaximo })
                 })
             }
             this.setState({ conceptos: respuesta.data })
