de2ca35e9d219b9a43d70400c26ce9db9d87aafd
Adan Gama <agama@certuit.com>
2021-07-29 21:41:07 -0700

    cambio state de fechas

--- src/Views/Viajes/AgregarViaje.js
+++ src/Views/Viajes/AgregarViaje.js
@@ -105,8 +105,8 @@ class AgregarViaje extends Component {
      handleAceptar = (e) => {
         e.preventDefault();
             var params = {
-                m_dFecha: this.fechaHoraRegistro.split("T")[0],
-                m_tHora: this.fechaHoraRegistro.split("T")[1],
+                m_dFecha: this.state.fechaHoraRegistro.split("T")[0],
+                m_tHora: this.state.fechaHoraRegistro.split("T")[1],
                 m_nIdEstatusViaje: this.state.estatusListado,
                 m_nIdSucursal : this.state.idSucursalAgregar,
                 m_sCandadoOficial : this.state.candadoOficial,
