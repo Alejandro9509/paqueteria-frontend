39bf8fd1275f3aaed362cadd64a35245e4289d30
Adan Gama <agama@certuit.com>
2021-07-29 21:28:33 -0700

    Cambios en viajes

---
+++ src/Util/Contexts/ViajesContext.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { trackPromise } from "react-promise-tracker";
+
+const headers = {
+    'Content-Type': 'application/json',
+    //    'access-control-allow-origin': '*'
+}
+
+function agregarViaje( params){
+    const url = `${process.env.REACT_APP_API_URL}/Viajes/Agregar`;
+    let result;
+    trackPromise(
+        result =  axios.post(url, Object.assign({}, params), { headers })
+        );
+    return result
+}
+
+
+
+
+export {agregarViaje }
