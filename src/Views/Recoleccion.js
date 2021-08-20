aafedec4eefef0d3596e8eec4743c12257e18cfa
Esteban Valencia <evalencia@certuit.com>
2021-08-19 18:14:42 -0700

    se me olvido descomentar codigo

--- src/Views/Recoleccion.js
+++ src/Views/Recoleccion.js
@@ -706,7 +706,7 @@ function Recoleccion() {
 
         console.log(params)
         console.log(JSON.stringify(params))
-        /*if (state.idRecoleccion != 0) {
+        if (state.idRecoleccion != 0) {
             modificarRecoleccion(state.idRecoleccion, params)
                 .then((respuesta) => {
                     showSuccess(respuesta.data);
@@ -737,7 +737,7 @@ function Recoleccion() {
                     console.log(err);
                     showSuccess(err);
                 });
-        }*/
+        }
 
 
     };
