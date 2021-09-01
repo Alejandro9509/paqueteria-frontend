595ffd1251172f1d440457996f93aba8351f422a
Esteban Valencia <evalencia@certuit.com>
2021-09-01 10:31:58 -0700

    Se resolvio bug que hacia que los productos en convenios no cambiaran al cambiar de tarifas

--- src/Views/Tarifas/ProductosTarifa.js
+++ src/Views/Tarifas/ProductosTarifa.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
@@ -49,6 +49,11 @@ export default function ProductosTarifa({productos = [], productosSeleccionados 
 
     const leftChecked = intersection(checked, left);
     const rightChecked = intersection(checked, right);
+
+    useEffect(value => {
+        setLeft(productos)
+        setRight(productosSeleccionados)
+    }, [productos], [productosSeleccionados])
 
     const handleToggle = (value) => () => {
         const currentIndex = checked.indexOf(value);
