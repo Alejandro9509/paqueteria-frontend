0d0186e09f12fb8e24ac944861581837437fba08
Adrian <arenteria@certuit.com>
2021-09-10 11:26:03 -0700

    Se agrego mas tiempo de refrescamiento

--- src/Views/UltimaMilla/UltimaMilla.js
+++ src/Views/UltimaMilla/UltimaMilla.js
@@ -123,7 +123,7 @@ class UltimaMilla extends Component {
         obtenerUltimaMillaFecha(date, idSucursal, zonas).then(({data}) => {
             if (data.m_nIdUltimaMilla !== 0) {
                 if (actualizar) {
-                    this.interval = setInterval(() => this.getFechaUltimaMilla(date, idSucursal, zonas, tipoBusqueda), 100000);
+                    this.interval = setInterval(() => this.getFechaUltimaMilla(date, idSucursal, zonas, tipoBusqueda), 30000);
                 }
                 if (!this.state.ultimaMilla) {
                     data.m_arrClsParadaUltimaMilla.forEach(t => t.color = randomColor(10))
