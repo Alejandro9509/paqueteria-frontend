fa10b2d47be3471b4a127dcc15bde7a9b3685e19
Esteban Valencia <evalencia@certuit.com>
2021-09-24 15:06:31 -0500

    Se agregan y modifican tarifas por zonas

--- src/Views/ZonasTarifas/ZonaTarifasAgregar.js
+++ src/Views/ZonasTarifas/ZonaTarifasAgregar.js
@@ -4,6 +4,14 @@ import {FormControl, Grid, InputLabel, Select} from "@material-ui/core";
 import Noty from "noty";
 import ZonaTarifasTabs from "./ZonaTarifasTabs";
 import {agregarZonaTarifa, modificarZonaTarifa, obtenerByIdZonaTarifa} from "../../Util/Contexts/ZonaTarifaContext";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import CodigosPostalesZonas from "../ZonasOperativas/CodigosPostalesZonas";
+import ConceptosAdicionalesRecoleccion from "../Tarifas/ConceptosAdicionalesRecoleccion";
+import ConceptosAdicionalesEntrega from "../Tarifas/ConceptosAdicionalesEntrega";
+import {getUniqueListBy} from "../../Util/Util";
 
 function showSuccess(mensaje) {
     new Noty({
@@ -14,19 +22,126 @@ function showSuccess(mensaje) {
     }).show()
 }
 
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+function not(a, b) {
+    return a.filter((value) => b.indexOf(value) === -1);
+}
+
 function ZonaTarifasAgregar({idZona, consult}) {
     const [state, setState] = useState({})
     const [selec, setSelec] = useState({})
+    const [value, setValue] = React.useState(0);
+    const [todosConceptos, setTodosConceptos] = useState([])
+    const [conceptosRecoleccion, setConceptosRecoleccion] = useState([])
+    const [conceptosEntrega, setConceptosEntrega] = useState([])
+
+    const handleChangeTab = (event, newValue) => {
+        setValue(newValue);
+    };
+
     useEffect(value => {
-        if (idZona){
+        if (idZona !== 0){
             obtenerByIdZonaTarifa(idZona).then(({data}) =>{
+                console.log(data)
                 setSelec(data)
+                let conceptosCast = []
+                data.m_arrArConceptos.forEach(element => {
+                    conceptosCast.push({
+                        idConcepto : element.m_nIdConceptosFacturacion,
+                        importe: element.m_cImporte,
+                        retiene: element.m_nIdImpuestoRetiene,
+                        traslada: element.m_nIdImpuestoTraslada,
+                        importeRet: element.m_cImporteRetiene,
+                        importeIVA: element.m_cImporteIva,
+                        rangoMinimo: element.m_xnRangoMinimo,
+                        rangoMaximo: element.m_xnRangoMaximo,
+                        nombreConcepto: element.m_sConcepto,
+                        tipoCalculo: element.m_nIdTipoCalculo,
+                        agregadoDesde: element.m_nIdAgregadoDesde,
+                        tipoMedida: element.m_nIdTipoMedida
+                    })
+                })
+
+                setTodosConceptos(conceptosCast)
             })
         }
     } ,[idZona])
 
     const handleDataCodigosPostalesChange = (data) => {
         setState(data)
+    }
+
+    const addConcepto = (data) => {
+        // const { conceptosRecoleccion, todosConceptos,conceptosAdicionales, conceptosManiobra, conceptosEntrega } = this.state
+        // let ivaTraslada = [];
+        // let ivaRetiene = [];
+        const concept = {
+            idConcepto : data.concepto.m_nIdConceptosFacturacion,
+            concepto: data.concepto,
+            importe: data.importe,
+            retiene: data.retiene,
+            traslada: data.traslada,
+            importeRet: data.importeRet,
+            importeIVA: data.importeIVA,
+            rangoMinimo: data.rangoMinimo,
+            rangoMaximo: data.rangoMaximo,
+            nombreConcepto: data.concepto.m_sConcepto,
+            tipoCalculo: data.tipoCalculo,
+            agregadoDesde: data.agregadoDesde,
+            tipoMedida: data.tipoMedida
+        }
+        let newArray = []
+        todosConceptos.forEach((i) => newArray.push(i))
+        newArray.push(concept)
+        // ivaTraslada = getUniqueListBy(todosConceptos, "traslada").map(i => i.traslada);
+        // ivaRetiene = getUniqueListBy(todosConceptos, "retiene").map(i => i.retiene);
+        /*if (data.agregadoDesde == 0){
+            conceptosAdicionales.push(concept)
+            this.setState({ conceptosAdicionales: conceptosAdicionales, todosConceptos: todosConceptos, ivaRetiene: ivaRetiene, ivaTraslada: ivaTraslada })
+        }else if (data.agregadoDesde == 1){
+            conceptosManiobra.push(concept)
+            this.setState({ conceptosManiobra: conceptosManiobra, todosConceptos: todosConceptos, ivaRetiene: ivaRetiene, ivaTraslada: ivaTraslada })
+        }else if (data.agregadoDesde == 2){
+            conceptosEntrega.push(concept)
+            this.setState({ conceptosEntrega: conceptosEntrega, todosConceptos: todosConceptos, ivaRetiene: ivaRetiene, ivaTraslada: ivaTraslada })
+        }else if (data.agregadoDesde == 3){
+            conceptosRecoleccion.push(concept)
+            this.setState({ conceptosRecoleccion: conceptosRecoleccion, todosConceptos: todosConceptos, ivaRetiene: ivaRetiene, ivaTraslada: ivaTraslada })
+        }*/
+        setTodosConceptos(newArray)
+    }
+
+    const removeConcepto = (item) => {
+        // const newArrayConceptos = conceptosRecoleccion.filter(c => this.filtrarConceptoAdicionalManiobraEmbarqueRecoleccion(c, item))
+        const newArrayTodosConceptos = todosConceptos.filter(c => c !== item)
+        // this.setState({ conceptosRecoleccion: newArrayTodosConceptos, todosConceptos: newArrayTodosConceptos })
+        setTodosConceptos(newArrayTodosConceptos)
     }
 
     const handleAceptar = (e) =>{
@@ -40,13 +155,26 @@ function ZonaTarifasAgregar({idZona, consult}) {
             m_sEstado: state.estado,
             m_sCodMunicipio: state.idMunicipio,
             m_sMunicipio: state.municipio,
-            m_arrCP: state.selectedCP
+            m_arrCP: state.selectedCP,
+            m_arrArConceptos: todosConceptos.map(c => ({
+                m_nIdConceptosFacturacion: c.idConcepto,
+                m_cImporte: c.importe,
+                m_nIdImpuestoTraslada: c.traslada,
+                m_nIdImpuestoRetiene: c.retiene,
+                m_cImporteRetiene: c.importeRet,
+                m_cImporteIva: c.importeIVA,
+                m_nIdTipoCalculo: c.tipoCalculo,
+                m_xnRangoMinimo: c.rangoMinimo,
+                m_xnRangoMaximo: c.rangoMaximo,
+                m_nIdAgregadoDesde: c.agregadoDesde,
+                m_nIdTipoMedida: c.tipoMedida
+            })),
         }
         console.log(JSON.stringify(params))
         if (state.idZona){
             modificarZonaTarifa(state.idZona, params).then(({data}) => {
                 showSuccess(data)
-                setSelec({})
+                setSelec(0)
             }).catch((err) => {
                 console.log(err);
                 showSuccess(err);
@@ -54,7 +182,7 @@ function ZonaTarifasAgregar({idZona, consult}) {
         }else {
             agregarZonaTarifa(params).then(({data}) => {
                 showSuccess(data)
-                setSelec({})
+                setSelec(0)
             }).catch((err) => {
                 console.log(err);
                 showSuccess(err);
@@ -73,10 +201,58 @@ function ZonaTarifasAgregar({idZona, consult}) {
                             </div>
                             <div className="widget-container">
                                 <div className="widget-content">
-                                    <ZonaTarifasTabs
+                                    <Box sx={{ width: '100%' }}>
+                                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                                            <Tabs value={value} onChange={handleChangeTab} aria-label="basic tabs example">
+                                                <Tab label="Códigos Postales" {...a11yProps(0)} />
+                                                <Tab label="Recoleccion" {...a11yProps(1)} />
+                                                <Tab label="Entrega" {...a11yProps(2)} />
+                                            </Tabs>
+                                        </Box>
+                                        <TabPanel value={value} index={0}>
+                                            <div className={'row'}>
+                                                <div className="widget-container">
+                                                    <div className="widget-content">
+                                                        <CodigosPostalesZonas
+                                                            seleccion={selec}
+                                                            onChange={handleDataCodigosPostalesChange}
+                                                            consult={consult}/>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </TabPanel>
+                                        <TabPanel value={value} index={1}>
+                                            <div className="widget-container">
+                                                <div className="widget-content">
+                                                    <ConceptosAdicionalesRecoleccion consult={consult}
+                                                                                     select={{}}
+                                                                                     conceptosAdicionales={todosConceptos.filter(i => i.agregadoDesde == 3)}
+                                                                                     addConcepto={addConcepto}
+                                                                                     removeConcepto={removeConcepto}
+                                                                                     ivaRetiene={[]}
+                                                                                     ivaTraslada={[]}/>
+                                                </div>
+                                            </div>
+
+                                        </TabPanel>
+                                        <TabPanel value={value} index={2}>
+                                            <div className="widget-container">
+                                                <div className="widget-content">
+                                                    <ConceptosAdicionalesEntrega consult={consult}
+                                                                                 select={{}}
+                                                                                 conceptosAdicionales={todosConceptos.filter(i => i.agregadoDesde == 2)}
+                                                                                 addConcepto={addConcepto}
+                                                                                 removeConcepto={removeConcepto}
+                                                                                 ivaRetiene={[]}
+                                                                                 ivaTraslada={[]}/>
+                                                </div>
+                                            </div>
+                                        </TabPanel>
+                                    </Box>
+                                    {/*<ZonaTarifasTabs
                                         selec={selec}
                                         handleDataCodigosPostalesChange={handleDataCodigosPostalesChange}
-                                        consult={consult}/>
+                                        consult={consult}/>*/}
                                 </div>
                             </div>
                             <div className="row">
