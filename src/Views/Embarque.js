bed7d4da961306dcc53cfe687d2e8a461fc05048
Esteban Valencia <evalencia@certuit.com>
2021-11-25 12:25:37 -0800

    Se agregaron campos para seguro

--- src/Views/Embarque.js
+++ src/Views/Embarque.js
@@ -1177,7 +1177,7 @@ function Embarque(props) {
         console.log(params)
         console.log(JSON.stringify(params))
 
-        /*if (state.idEmbarque != 0) {
+        if (state.idEmbarque != 0) {
             modificarEmbarques(state.idEmbarque, params)
                 .then((respuesta) => {
                     showSuccess(respuesta.data);
@@ -1212,7 +1212,7 @@ function Embarque(props) {
                     console.log(err);
                     showSuccess(err);
                 });
-        }*/
+        }
     };
 
     function handleSelectCP(id, cp) {
