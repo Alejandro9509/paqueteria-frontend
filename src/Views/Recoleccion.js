40397b71d663f2d1c54fb86cb953deff312005fd
Adrian <arenteria@certuit.com>
2021-11-27 11:09:23 -0600

    Corrección al crear recolección

--- src/Views/Recoleccion.js
+++ src/Views/Recoleccion.js
@@ -880,7 +880,7 @@ function Recoleccion() {
 
         console.log(params)
         console.log(JSON.stringify(params))
-   /*     if (state.idRecoleccion != 0) {
+        if (state.idRecoleccion != 0) {
             modificarRecoleccion(state.idRecoleccion, params)
                 .then((respuesta) => {
                     showSuccess(respuesta.data);
@@ -903,7 +903,7 @@ function Recoleccion() {
                     console.log(err);
                     showSuccess(err);
                 });
-        }*/
+        }
 
     };
 
