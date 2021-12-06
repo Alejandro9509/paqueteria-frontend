09ab561dcf66dfeba1c564b1d449d35e29a22420
Esteban Valencia <evalencia@certuit.com>
2021-12-05 18:07:23 -0600

    Se ocultó el cotizador de momento

--- src/Views/Embarque.js
+++ src/Views/Embarque.js
@@ -4847,7 +4847,7 @@ function Embarque(props) {
                                         }
                                     </div>
 
-                                    <div className="row">
+                                    {/*<div className="row">
                                         <Cotizador embarque={state} remitente={remitente} paquetes={dataPaquetes.map(p =>({
                                             Tipo: p.m_nIdTipo,
                                             Peso: p.m_rPeso,
@@ -4860,7 +4860,7 @@ function Embarque(props) {
                                             ctd:p.m_nCantidad,
                                             IdProducto:p.m_nIdProducto
                                         }))} destinatario={destinatario}/>
-                                    </div>
+                                    </div>*/}
 
                                 </div>
                                 <div className="form-footer ol-md-12">
