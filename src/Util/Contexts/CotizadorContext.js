043ca45806cdc8bf068bb1925ab1883602a2756b
Adrian <arenteria@certuit.com>
2021-12-10 07:45:27 -0600

    Ajustes en cotizador

--- src/Util/Contexts/CotizadorContext.js
+++ src/Util/Contexts/CotizadorContext.js
@@ -7,13 +7,14 @@ const headers = API_HEADERS
 function obtenerCotizacion( data, paquetes, remitente, destinatario, recoleccion) {
     const url = `${process.env.REACT_APP_API_URL}/Cotizador/Agregar`;
     let result;
+    console.log(destinatario)
     var params = {
         IdOrigen: remitente.origenRemitente.m_nIdCiudad,
         IdDestino: destinatario.destinoDestinatario.m_nIdCiudad,
         IdEmbarque: data.idEmbarque,
         IdRecoleccion: data.idRecoleccion,
-        IdZonaEntrega: remitente.zonaTarifaDestinatario ?  remitente.zonaTarifaDestinatario.m_nIdZona : 0,
-        IdZonaRecoleccion: destinatario.zonaTarifaRemitente ? destinatario.zonaTarifaRemitente.m_nIdZona : 0,
+        IdZonaEntrega: destinatario.zonaTarifaDestinatario ?  destinatario.zonaTarifaDestinatario.m_nIdZona : 0,
+        IdZonaRecoleccion: remitente.zonaTarifaRemitente ? remitente.zonaTarifaRemitente.m_nIdZona : 0,
         IdCliente: data.clientePaga.m_nIdCliente,
         EntregaEnSucursal:  data.entregaEnSucursal,
         IdSeguro: data.idTipoSeguro,
