0c2b1f3bed0b65206b6ab772c13f2bbf6c88aa16
Esteban Valencia <evalencia@certuit.com>
2021-12-11 12:03:00 -0600

    conectado a url de desarrollo

--- src/Util/Contexts/ProductosContext.js
+++ src/Util/Contexts/ProductosContext.js
@@ -78,7 +78,7 @@ function obtenerProductos(){
     return result
 }
 function obtenerProductosByConvenioCliente(idCliente){
-    const url = `${process.env.REACT_APP_API_URL_LOCAL}/api/Productos/GetByConvenioCliente/${idCliente}`;
+    const url = `${process.env.REACT_APP_REPORT_URL}/api/Productos/GetByConvenioCliente/${idCliente}`;
     let result;
     trackPromise(
         result =  axios.get(url, { headers })
