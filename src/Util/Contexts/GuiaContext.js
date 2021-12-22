495d0d86fdd2185dbfc263f1372ad8b57a36479d
Esteban Valencia <evalencia@certuit.com>
2021-12-22 13:59:21 -0700

    Se agregó dialogo para cambiar estatus de la guia

--- src/Util/Contexts/GuiaContext.js
+++ src/Util/Contexts/GuiaContext.js
@@ -84,6 +84,15 @@ function cambiarTipoCobro(idGuia, tipoCobro) {
     return result
 }
 
+function cambiarEstatusGuia(idGuia, estatus) {
+    const url = `${process.env.REACT_APP_API_URL}/Guias/CambiarEstatusGuia/${idGuia}/${estatus}`;
+    let result;
+    trackPromise(
+        result =  axios.put(url, Object.assign({}, {}), { headers })
+    );
+    return result
+}
+
 function obtenerGuiaPendientes(idOrigen, idDestino) {
     const url = `${process.env.REACT_APP_API_URL}/Guia/GetListadoPendientes/` +
         idOrigen +
@@ -177,6 +186,6 @@ function obtenerGuiasFiltroCorteCaja(fecha, destino, idMoneda, idTipoPago) {
     return result
 }
 
-export { obtenerGuiasFiltroCorteCaja, entregaOcurreGuia, modificarGuia, agregarGuia, eliminarGuia, obtenerGuiaId,
+export {cambiarEstatusGuia, obtenerGuiasFiltroCorteCaja, entregaOcurreGuia, modificarGuia, agregarGuia, eliminarGuia, obtenerGuiaId,
     obtenerGuia, ultimoFolioGuia, cancelarGuia, obtenerGuiasFiltro, obtenerGuiaPendientes, imprimirGuia,
     obtenerGuiaUltimaMilla, reasignarGuia, obtenerGuiaReporte , cambiarTipoCobro}
