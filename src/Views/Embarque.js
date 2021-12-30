6ca09f26c8eee3db20980a1264fd82c4d302ac58
Esteban Valencia <evalencia@certuit.com>
2021-12-29 18:04:54 -0700

    Se implementó el parametro de limpiar producto al agregar embarque

--- src/Views/Embarque.js
+++ src/Views/Embarque.js
@@ -3687,6 +3687,7 @@ function Embarque(props) {
                                             onChangeList={handleListPaquetesChange}
                                             disabled={state.agregar === "Consultar" || state.embarqueConGuia}
                                             cliente={state.clientePaga}
+                                            LimpiarProducto={configuraciones.limpiarProducto}
                                         />
 
                                     </div>
