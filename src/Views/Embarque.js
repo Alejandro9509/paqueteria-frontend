6e35467471b7052d009c94ca0b93aba7a5887759
Esteban Valencia <evalencia@certuit.com>
2022-01-10 15:32:14 -0700

    Cambios en el submit de embarques

--- src/Views/Embarque.js
+++ src/Views/Embarque.js
@@ -2799,7 +2799,7 @@ function Embarque(props) {
                         <div id="Agregar"
                              className={props.location.idRecoleccion != undefined ? "tab-pane fade in show" : "tab-pane fade"}>
 
-                            <form className="j-forms row" >
+                            <form className="j-forms row" onSubmit={handleAceptar} onKeyDown={e => {if (e.code === 13){e.preventDefault()}}}>
                                 <div className="form-content">
                                     {/*<div
                                         className="wizard-breadcrumb number-style"
@@ -3692,8 +3692,7 @@ function Embarque(props) {
                                                     color={"primary"}
                                                     variant={"contained"}
                                                     type="submit"
-                                                    disabled={state.agregar === "Consultar"}
-                                                    onClick={handleAceptar}>
+                                                    disabled={state.agregar === "Consultar"}>
                                                 Guardar embarque
                                             </Button>
                                         </Grid>
