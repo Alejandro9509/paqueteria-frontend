98e8e6377903b9cc7797d8c1d68beac0fa40c3d4
Erick Castillo Romero <ecastillo@certuit.com>
2022-01-19 13:51:53 -0800

    Se agrega el municipio relacionado al codigo postal

--- src/Views/ZonasOperativas/CPTransferList.js
+++ src/Views/ZonasOperativas/CPTransferList.js
@@ -25,7 +25,6 @@ export default function CPTransferList({allItems = [],selectedItems = [],onChang
     const [checked, setChecked] = React.useState([]);
     const [left, setLeft] = React.useState(allItems);
     const [right, setRight] = React.useState(selectedItems);
-    const [municipios, setMunicipios] = React.useState([]);
     const leftChecked = intersection(checked, left);
     const rightChecked = intersection(checked, right);
 
@@ -81,13 +80,6 @@ export default function CPTransferList({allItems = [],selectedItems = [],onChang
         onChange(leftt, rightt)
     };
 
-    useEffect(value=>{
-         var municipios = right.map(function(item){ return item.m_sMunicipio });
-         let result = municipios.filter((item,index)=>{
-            return municipios.indexOf(item) === index;
-          })
-          setMunicipios(result)
-    },[])
 
     const customList = (items) => (
         <Paper style={{ width: '100%', height: 500, overflow: 'auto' }}>
@@ -113,7 +105,7 @@ export default function CPTransferList({allItems = [],selectedItems = [],onChang
                                     }}
                                 />
                             </ListItemIcon>
-                            <ListItemText id={labelId} primary={`${value.m_sCP} - ${value.m_sColonia}`} />
+                            <ListItemText id={labelId} primary={`${value.m_sCP} - ${value.m_sColonia} - ${value.m_sMunicipio}`} />
                         </ListItem>
                     );
                 })}
@@ -126,7 +118,7 @@ export default function CPTransferList({allItems = [],selectedItems = [],onChang
         <div align={'center'}>                
             <Grid container spacing={2} justifyContent="center" alignItems="center">
                 
-                <Grid item xs={4}><Typography variant="h3" component="h2"> Códigos postales disponibles para relacionar</Typography>{customList(left)}</Grid>
+                <Grid item xs={5}><Typography variant="h3" component="h2"> Códigos postales disponibles para relacionar</Typography>{customList(left)}</Grid>
                 <Grid item xs={1}>
                     <Grid container direction="column" alignItems="center">
                         <Button
@@ -171,26 +163,8 @@ export default function CPTransferList({allItems = [],selectedItems = [],onChang
                         </Button>
                     </Grid>
                 </Grid>
-                <Grid item xs={4}><Typography variant="h3" component="h2">Códigos postales ya relacionados a la zona</Typography>{customList(right)}</Grid>
-                <Grid item xs={3} >
-                <Paper style={{ width: '100%', height: 530, overflow: 'auto' }}>
-                 <Typography variant="h3" component="h2" >
-                   Municipios Relacionados
-                 </Typography>
-                 <div >
-                { municipios.map(m=>{
-                    return ( 
-                    <List dense component="div" role="list">
-                    <ListItem> 
-                     <ListItemText
-                       primary={m}
-                      />
-                    </ListItem>   
-                   </List>)
-               })  }
-                 </div>
-                 </Paper>
-             </Grid>
+                <Grid item xs={5}><Typography variant="h3" component="h2">Códigos postales ya relacionados a la zona</Typography>{customList(right)}</Grid>
+
             </Grid>
         </div>
 
