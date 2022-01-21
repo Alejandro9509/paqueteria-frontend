fecc4076d3b1eb62fd1e42249e4661bafa9d6768
Esteban Valencia <evalencia@certuit.com>
2022-01-21 15:51:38 -0600

    Ajuste que hacia que al cancelar el agregar un tipo de cobro se reseteara la info

--- src/Views/Tarifas/TipoCobro.js
+++ src/Views/Tarifas/TipoCobro.js
@@ -148,9 +148,8 @@ class TipoCobro extends Component {
                                         </div>
                                     </div>
                                     <div className="col-sm-12 col-md-12 unit">
-                                        <button href="#Listado" role="tab" data-toggle="tab" className="btn btn-secondary secondary-btn"
-                                            onClick={this.handleClose}
-                                        >
+                                        <button data-toggle="tab" className="btn btn-secondary secondary-btn"
+                                            onClick={this.handleClose}>
                                             Cancelar</button>
                                         <button onClick={this.handleAceptar} className="btn btn-primary primary-btn">Aceptar</button>
                                     </div>
