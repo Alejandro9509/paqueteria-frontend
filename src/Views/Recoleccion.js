32645df7a17c5c5d52210897b2336decb4924903
Erick Castillo Romero <ecastillo@certuit.com>
2022-01-26 18:08:44 -0800

    Correccion confirmacion recoleccion

--- src/Views/Recoleccion.js
+++ src/Views/Recoleccion.js
@@ -817,8 +817,7 @@ function Recoleccion() {
     const mostrarCotizadorRec = (isVisible) =>{
         setState({
             ...state,
-            mostrarCotizador:isVisible,
-            clientePaga: {m_nNumeroCliente: 'No. Cliente', m_sNombreFiscal: 'Nombre fiscal'},
+            mostrarCotizador:isVisible
         })
     }
 
@@ -1063,6 +1062,7 @@ function Recoleccion() {
         params.m_nIdCotizacion = state.idCotizacion
     //    console.log(params)
         console.log(JSON.stringify(params))
+        console.log(coordenadas)
      if (state.idRecoleccion != 0) {
             modificarRecoleccion(state.idRecoleccion, params)
                 .then((respuesta) => {
@@ -1087,21 +1087,28 @@ function Recoleccion() {
                              //   console.log(respuesta.data);
                                 showSuccess(respuesta.data);
                                 limpiarInputsAgregar()
+                                mostrarDialogoMapa(false)
                                 confirmAlert({
                                     title: 'Confirmación',
                                     message: '¿Desea crear otra recoleccion?',
                                     buttons: [
                                         {
                                             label: 'Sí',
-                                            onClick: ()=>{                                  
+                                            onClick: ()=>{//limpia los inputs para volver a agregar denuevo la info            
+                                               
+                                                setLimpiarRemDes(e) 
+                                                mostrarCotizadorRec(false)
+                                                limpiarInputsAgregar()
+                                                                                
+                                                $('.tab-content div ').removeClass('in show');
+                                                $('#Agregar').addClass('in show');   
+                                                setTabActiva(1)                                               
                                               
-                                                mostrarCotizadorRec(false)//deja de mostrar el cotizador de la recoleccion pasada
-                                                setLimpiarRemDes(e)
                                             }
                                         },
                                         {
                                             label: 'No',
-                                            onClick: ()=>{return}
+                                            onClick: ()=>{ handleShowListado();}
                                         }
                                     ]
                                 });
@@ -1633,7 +1640,6 @@ function Recoleccion() {
             return {
                 ...state,
                 idRecoleccion:0,
-                idSucursalAgregar: '',
                 folioRecoleccion: '',
                 folioEmbarque: '',
                 folioGuia: '',
