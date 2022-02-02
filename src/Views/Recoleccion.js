ae9ff82f1ef2ae0b6d11c2a841d67f7ba803e836
Erick Castillo Romero <ecastillo@certuit.com>
2022-02-01 17:47:40 -0800

    Correccion validacion confirmacionUbicacion en diferente domicilio recoleccion

--- src/Views/Recoleccion.js
+++ src/Views/Recoleccion.js
@@ -767,13 +767,14 @@ function Recoleccion() {
     }
 
     const validarCoordenadas = (coordenadas) => {
+        console.log("coordenadas"+coordenadas)
         /**Si es modificacion*/
         if (state.idRecoleccion != 0){
             /**Si es recoleccion diferente domicilio y no hay coordenadas guardadas*/
             if(state.diferenteRecoleccion
                 && !isValidText(recoleccionDD.latitudRec)
-                && !isValidText(recoleccionDD.longitudRec)
-                && !coordenadas){
+                && !isValidText(recoleccionDD.longitudRec))
+                {
                 mostrarDialogoMapa(true)
 
                 return false
@@ -789,14 +790,14 @@ function Recoleccion() {
             /**Si es agregar*/
         }else{
             /**Si es entrega diferente domicilio y no hay coordenadas guardadas*/
-            if (state.diferenteRecoleccion  && !coordenadas){
+            if (state.diferenteRecoleccion){
                 mostrarDialogoMapa(true)
 
                 return false
                 /**Si es recoleccion en el domicilio del remitente y no hay coordenadas*/
                 debugger;
             }else if (!state.diferenteRecoleccion
-                && !isValidText(remitente.latitudR)
+                && !isValidText(remitente.latitudR) 
                 && !isValidText(remitente.longitudR)
                 && !coordenadas){
                 mostrarDialogoMapa(true)
