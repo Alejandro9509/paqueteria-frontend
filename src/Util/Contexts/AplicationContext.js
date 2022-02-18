6f0dd4b7017373bda221cb8db18a8c059c123ff1
Adrian Renteria <arenteria@certuit.com>
2022-02-18 08:33:19 -0800

    Se ajusto el tiempo de inactividad a 15 min

--- src/Util/Contexts/AplicationContext.js
+++ src/Util/Contexts/AplicationContext.js
@@ -45,7 +45,7 @@ export class AplicationProvider extends Component{
                     <IdleTimer
                         element={document}
                         ref={ref => { this.idleTimer = ref }}
-                        timeout={1000 * 60 * 5}
+                        timeout={1000 * 60 * 15}
                         onActive={this.handleOnActive}
                         onIdle={this.handleOnIdle}
                         onAction={this.handleAction}
