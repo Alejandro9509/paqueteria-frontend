542e9a1f4ce5f0ed9209f8b15d45990ef492b53b
Esteban Valencia <evalencia@certuit.com>
2022-04-22 17:00:36 -0600

    Se quitó validacion de zonas para viajes locales. Se agregó calidacion de productos para viajes locales.

--- src/Views/Tarifas/CrearTarifaRangos.js
+++ src/Views/Tarifas/CrearTarifaRangos.js
@@ -304,19 +304,19 @@ export default function CrearTarifaRangos(props) {
     }
     /**Filtra los productos para que solo queden los que no se han usado en otro viaje local con la misma sucursal, concepto y zona*/
     const filtrarProductosViajeLocal = (viaje) => {
-        let zonasDisponibles = []
-        zonasListado.forEach(i => {
-            zonasDisponibles.push(i)
+        let productosDisponibles = []
+        productosListado.forEach(i => {
+            productosDisponibles.push(i)
         })
         let otrosViajes = viajesLocalesListado.filter(v => v.idViaje !== viaje.idViaje)
         otrosViajes = otrosViajes.filter(v => v.idSucursal === viaje.idSucursal && v.idConcepto === viaje.idConcepto)
 
         otrosViajes.forEach(v => {
-            v.zonas.forEach(z => {
-                zonasDisponibles = zonasDisponibles.filter(j => j.m_nIdZona !== z.m_nIdZona)
+            v.productos.forEach(z => {
+                productosDisponibles = productosDisponibles.filter(j => j.m_nIdProducto !== z.m_nIdProducto)
             })
         })
-        return zonasDisponibles
+        return productosDisponibles
     }
 
     /**Filtra los conceptos para que solo queden las que no se han usado en otro viaje local con la misma sucursal*/
@@ -683,9 +683,9 @@ export default function CrearTarifaRangos(props) {
                                 tiposCalculoListado={tiposCalculoListado}
                                 unidadesMedidaListado={unidadesMedidaListado}
                                 handleDeleteViajeLocal={handleDeleteViajeLocal}
-                                zonasListado={filtrarZonasViajeLocal(viaje)}
+                                zonasListado={zonasListado}
                                 onRequestZonasBySucursal={handleOnRequestZonasBySucursal}
-                                productosListado={productosListado}
+                                productosListado={filtrarProductosViajeLocal(viaje)}
                                 disabled={props.disabled}
                                 showDialogZonas={showDialogZonas}
                                 handleShowDialogZonas={handleShowDialogZonas}
