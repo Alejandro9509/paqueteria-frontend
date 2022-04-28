23dbdf04d9940ce976991abb3cbb0b9d7a029964
Adrian Renteria <arenteria@certuit.com>
2022-04-28 10:12:15 -0700

    Error en validar escaner

--- src/Views/Viajes.js
+++ src/Views/Viajes.js
@@ -838,7 +838,7 @@ function Viajes() {
     const showSalidaDialog = (data) => {
           validarSalidaParada(data.m_nIdViaje).then((respuesta)=>{
               let encontrado = respuesta.data.find(parada=>parada.Timbrado==false)
-              let qr = respuesta.data.find(parada=>parada.QR==false)
+              let qr = respuesta.data.find(parada=>parada.Escaneado==false)
 
               if(encontrado){//si encontro valor falso en timbrado
                   showSuccess(`No se puede marcar salida ya que no se ha generado CFDI para el folio: ${encontrado.FolioInforme}`)
