83832c048fbbb0030ad1023a05d033d5036ff43c
Adrian Renteria <arenteria@certuit.com>
2022-05-20 11:42:59 -0700

    Error al agregar viaje

--- src/Views/Viajes/AgregarViaje.js
+++ src/Views/Viajes/AgregarViaje.js
@@ -310,7 +310,7 @@ class AgregarViaje extends Component {
             m_arrInformes: this.state.dataInformesAsignados,
             m_nIdOrigen: this.state.idCiudadOrigen.m_nIdCiudad,
             m_nDestino: this.state.idCiudadDestino.m_nIdCiudad,
-            IdRemolque1: this.state.IdRemolque1.m_nIdUnidad,
+            IdRemolque1: this.state.IdRemolque1 ? this.state.IdRemolque1.m_nIdUnidad : 0,
             IdRemolque2: this.state.IdRemolque2 ? this.state.IdRemolque2.m_nIdUnidad : 0,
             IdDolly: this.state.IdDolly ? this.state.IdDolly.m_nIdUnidad : 0,
             m_nIdRuta: this.state.idRuta,
