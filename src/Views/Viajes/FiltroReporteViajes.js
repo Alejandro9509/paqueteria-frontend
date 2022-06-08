c6b6544717ba8201583a9a26bd043fe940ded9a8
Adrian Renteria <arenteria@certuit.com>
2022-06-08 16:18:26 -0700

    Se ajustaron las fechas por default

--- src/Views/Viajes/FiltroReporteViajes.js
+++ src/Views/Viajes/FiltroReporteViajes.js
@@ -18,9 +18,9 @@ class FiltroReporteViajes extends Component {
 
     constructor(props) {
         super(props);
-        const today = new Date();
+        var today = new Date();
         this.state = {
-            fechaInicial: today.getFullYear() + "-" + ((today.getMonth() - 3) <= 9 ? ("0" + (today.getMonth() - 3)) : (today.getMonth() - 3)) + "-" + (today.getDate() <= 9 ? ("0"+today.getDate()) : today.getDate()),
+            fechaInicial: today.getFullYear() + "-" + ((today.getMonth() + 1) <= 9 ? ("0" + (today.getMonth() + 1)) : (today.getMonth() + 1)) + "-01",
             fechaFinal: today.getFullYear() + "-" + ((today.getMonth() +1) <= 9 ? ("0"+(today.getMonth() +1)) : (today.getMonth() +1)) + "-" + (today.getDate() <= 9 ? ("0"+today.getDate()) : today.getDate()),
             sucursales: [],
             sucursalesSeleccionadas: []
