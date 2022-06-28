95e351c453fa190eece1f677c190ac0a1225911d
Esteban Valencia <evalencia@certuit.com>
2022-06-28 13:03:00 -0600

    * Se agrego soporte para mostrar mensaje de error al agregar guia

--- src/Views/Embarque.js
+++ src/Views/Embarque.js
@@ -2666,7 +2666,7 @@ function Embarque(props) {
                 handleShowListado()
             }).catch(err => {
                 console.log(err)
-                showSuccess(err)
+                showSuccess(err.response?.data)
                 handleShowListado()
             });
         } else {
