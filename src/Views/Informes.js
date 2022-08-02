4b5216f623f5151bfb70e783a5d287e26ecffbd6
Esteban Valencia <evalencia@certuit.com>
2022-08-02 14:51:19 -0600

    integracionRutaInforme. Mensaje de error al eliminar

--- src/Views/Informes.js
+++ src/Views/Informes.js
@@ -1168,7 +1168,7 @@ function Informes({history}) {
                         showSuccess(data)
                     })
                     .catch((err) => {
-                        showSuccess(err);
+                        showSuccess(err.response?.data);
                     });
             })
             .catch((err) => {
