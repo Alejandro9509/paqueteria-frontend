c8499eb1046e1f0b5083e921c7e46d6aa13486ee
Esteban Valencia <evalencia@certuit.com>
2022-08-17 14:47:58 -0600

    20220621-DENISSE-546. Se ajusto validacion para permitir salir viaje sin timbrar, a permisionarios

--- src/Views/Viajes.js
+++ src/Views/Viajes.js
@@ -899,12 +899,14 @@ function Viajes() {
     const showSalidaDialog = (e,data) => {
         e.stopPropagation()
           validarSalidaParada(data.m_nIdViaje).then((respuesta)=>{
-              let encontrado = respuesta.data.find(parada=>parada.Timbrado==false)
-             let qr = respuesta.data.find(parada=>parada.Escaneado==false)
+              if (!viajeSeleccionado.m_bUnidadPermisionario){
+                  let encontrado = respuesta.data.find(parada=>parada.Timbrado==false)
+                  let qr = respuesta.data.find(parada=>parada.Escaneado==false)
 
-              if(encontrado){//si encontro valor falso en timbrado
-                  showSuccess(`No se puede marcar salida ya que no se ha generado CFDI para el folio: ${encontrado.FolioInforme}`)
-                  return
+                  if(encontrado){//si encontro valor falso en timbrado
+                      showSuccess(`No se puede marcar salida ya que no se ha generado CFDI para el folio: ${encontrado.FolioInforme}`)
+                      return
+                  }
               }
 
             //  if(qr){//si encontro valor falso en qr
