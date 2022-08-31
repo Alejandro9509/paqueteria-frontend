4c774456f1709d659da7f2867c4f1261e4521abd
Esteban Valencia <evalencia@certuit.com>
2022-08-31 00:38:42 -0600

    ORLANDO-568. Ajuste al seleccionar CP

--- src/Views/DiferenteDomicilio/DiferenteDomicilioForm.js
+++ src/Views/DiferenteDomicilio/DiferenteDomicilioForm.js
@@ -97,12 +97,14 @@ export default function DiferenteDomicilioForm(props){
                         showSuccess(`No aplican entregas en la zona operativa`)
                         setState({
                             ...state,
-                            [input]: null
+                            [input]: null,
+                            zonaOperativa: null
                         })
                        }else{
                         setState(state => {
                             return {
                                 ...state,
+                                [input]: newValue,
                                 zonaOperativa: data[0]
                             }
                         })
@@ -113,7 +115,7 @@ export default function DiferenteDomicilioForm(props){
                     setState(state => {
                         return{
                             ...state,
-                            zonaOperativa: {}
+                            zonaOperativa: null
                         }
                     })
                 }
