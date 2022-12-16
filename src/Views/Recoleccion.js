3c02666c9c8d47cdca46a5bd64a4c1a025666eb3
Erick Castillo Romero <ecastillo@certuit.com>
2022-12-15 18:34:25 -0800

    validacion destinatario y filtrado de sucursales

--- src/Views/Recoleccion.js
+++ src/Views/Recoleccion.js
@@ -1782,6 +1782,11 @@ function Recoleccion() {
     };
     const handleEntregaEnSucursalCheckbox = (event) => {
         setRepetirConceptos(true)
+        if(destinatario.idDestinatario === ''){//Evita que cambie de estatus el check de entrega en sucursal
+            showSuccess("Se requiere seleccionar Destinatario")
+            return
+        }
+       
         setState({
             ...state,
             entregaEnSucursal: !state.entregaEnSucursal,
@@ -4049,7 +4054,7 @@ function Recoleccion() {
                                                                                         name: "idSucursalEntrega",
                                                                                     }}
                                                                                 >
-                                                                                    {dataSucursal.map((sucursal) => (
+                                                                                    {dataSucursal.filter(suc=>suc.m_nIdEstado == destinatario.estadoDestinatario).map((sucursal) => (
                                                                                         <MenuItem
                                                                                             key={sucursal.m_nIdSucursal}
                                                                                             value={sucursal.m_nIdSucursal}
