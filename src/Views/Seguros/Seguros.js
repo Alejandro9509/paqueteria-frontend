6667e59bf0df9bd1667623159870cc4a0cb28ec1
Esteban Valencia <evalencia@certuit.com>
2023-01-04 09:44:13 -0700

    Correcciones ortográficas

--- src/Views/Seguros/Seguros.js
+++ src/Views/Seguros/Seguros.js
@@ -84,7 +84,7 @@ function Seguros() {
                 width: 250,
               },
             {
-                headerName: "Poliza",
+                headerName: "Póliza",
                 field: "m_sPoliza",
                 width: 250,
               }
