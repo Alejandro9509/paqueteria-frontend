5d39fab6808dafd43b334877e3c5fe5c6acdb06b
Esteban Valencia <evalencia@certuit.com>
2023-01-26 18:04:37 -0700

    feat: se terminó procesado de datos de plantilla lineal

--- src/Views/Embarque/ImportarEmbarques.js
+++ src/Views/Embarque/ImportarEmbarques.js
@@ -14,10 +14,11 @@ import {
 
 import MenuItem from "@material-ui/core/MenuItem";
 import {importarProductos} from "../../Util/Contexts/ProductosContext";
-import {showSuccess,
+import {
+    showSuccess,
     getCurrentDate,
     getCurrentTime,
-    readExcel, DEFAULT_FORMAT
+    readExcel, DEFAULT_FORMAT, readExcelPlantillaLineal
 } from "../../Util/Util";
 import {FilePond} from "react-filepond";
 import 'filepond/dist/filepond.min.css';
@@ -93,27 +94,52 @@ function ImportarEmbarques(props) {
             return
         }
         obtenerPlantillaImportacionByIdCliente(state.cliente.m_nIdCliente).then(respuesta => {
-            readExcel(respuesta.data.data,files[0].file).then((resultado)=>{
-                resultado.forEach(item => item.idCliente = state.cliente.m_nIdCliente)
-                let params = {
-                    embarques: resultado
-                }
-                console.log(resultado)
-                console.log(params)
-                validarEmbarquesImportados(params).then(respuesta => {
-                    console.log(respuesta.data)
-                    setState({
-                        ...state,
-                        embarques: respuesta.data
+            if (respuesta.data.data.idTipoPlantilla === 1){
+                readExcel(respuesta.data.data,files[0].file).then((resultado)=>{
+                    resultado.forEach(item => item.idCliente = state.cliente.m_nIdCliente)
+                    let params = {
+                        embarques: resultado
+                    }
+                    console.log(resultado)
+                    console.log(params)
+                    validarEmbarquesImportados(params).then(respuesta => {
+                        console.log(respuesta.data)
+                        setState({
+                            ...state,
+                            embarques: respuesta.data
+                        })
+                    }).catch((error)=>{
+                        // showMessage(err,2000,"warning")
+                        console.log('error al validar: ' + error)
                     })
-                }).catch((error)=>{
+                }).catch((err)=>{
                     // showMessage(err,2000,"warning")
-                    console.log('error al validar: ' + error)
+                    console.log('error al importar' + err)
                 })
-            }).catch((err)=>{
-                // showMessage(err,2000,"warning")
-                console.log('error al importar' + err)
-            })
+            }else{
+                readExcelPlantillaLineal(respuesta.data.data,files[0].file).then((resultado)=>{
+                    resultado.forEach(item => item.idCliente = state.cliente.m_nIdCliente)
+                    let params = {
+                        embarques: resultado
+                    }
+                    console.log(resultado)
+                    console.log(params)
+                    validarEmbarquesImportados(params).then(respuesta => {
+                        console.log(respuesta.data)
+                        setState({
+                            ...state,
+                            embarques: respuesta.data
+                        })
+                    }).catch((error)=>{
+                        // showMessage(err,2000,"warning")
+                        console.log('error al validar: ' + error)
+                    })
+                }).catch((err)=>{
+                    // showMessage(err,2000,"warning")
+                    console.log('error al importar' + err)
+                })
+            }
+
         })
 
     }
