a40f5716ca20348e9b0fb15c7ed90f4c7ca7427b
Esteban Valencia <evalencia@certuit.com>
2023-02-07 22:46:18 -0700

    fix: Ajuste al recibir dato referencia

--- src/Views/Embarque.js
+++ src/Views/Embarque.js
@@ -1829,7 +1829,7 @@ function Embarque(props) {
                 recoleccionConCita: respuesta.data.m_bRecoleccionConCita,
                 //observaciones
                 observaciones: respuesta.data.m_sObservaciones,
-                referencia: respuesta.data.m_sRefencia,
+                referencia: respuesta.data.m_sReferencia,
             }
         });
     }
