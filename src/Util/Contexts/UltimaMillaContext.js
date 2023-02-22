40ec33fbcf6f61e7e2a2df1b059c7f49349de228
Esteban Valencia <evalencia@certuit.com>
2023-02-22 14:27:05 -0700

    fix: se cambio la cantidad de tiempo estimado por parada en um

--- src/Util/Contexts/UltimaMillaContext.js
+++ src/Util/Contexts/UltimaMillaContext.js
@@ -43,7 +43,7 @@ async function convertData(trucks, guias) {
                                 "lat": parseFloat(p.lat),
                                 "lng": parseFloat(p.lng)
                             },
-                            "duration": 6000,
+                            "duration": 0,
                             "tag": "Index_"+index
                         }],
 
