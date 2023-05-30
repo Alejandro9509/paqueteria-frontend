870a91b1c77e25956ed7d66f019a7c491fdf3dd0
Ana Karla calero Chacon <acalero@certuit.com>
2023-05-30 13:54:30 -0700

    Integracion de funciones y componentes para la impresion de reporte Guia Etiqueta formato webdev en la vista de Guia

--- src/Views/Guia.js
+++ src/Views/Guia.js
@@ -307,16 +307,23 @@ function Guia(props) {
         detallesPaquetesEtiquetas:[],
         receptorGuia:[],
         referencia:[],
+        reporteSeleccionado:{}
 
     })
     const [openDialog, setOpenDialog] = useState(false)
     const [dataReportes, setDataReportes] = useState([])
     const [seleccion, setSeleccion] = useState(null)
+    const [openDialogEtiqueta, setOpenDialogEtiqueta] = useState(false)
+    const [dataReportesEtiqueta, setDataReportesEtiqueta] = useState([])
+    const [seleccionEtiqueta, setSeleccionEtiqueta] = useState(null)
 
     useEffect(()=>{
 
         obtenerFormatosImpresionProceso(212).then(({data}) => {
             setDataReportes(data)
+        })
+        obtenerFormatosImpresionProceso(213).then(({data}) => {
+            setDataReportesEtiqueta(data)
         })
     }, [])
     const columns = React.useMemo(() => [
@@ -367,7 +374,7 @@ function Guia(props) {
 
                         </Tooltip>
                         <Tooltip title="Descargar PFD con etiquetas" disabled={!validarDerecho(9101465)}>
-                            <a className="btn btn-default btn-xs" onClick={() => generarReporteEtiqueta(row.row.m_nIdGuia, row.row.m_nFolioGuia)}>
+                            <a className="btn btn-default btn-xs" onClick={() => generarReporteEtiqueta(row.row)}>
                                 <i className="zmdi zmdi-inbox" style={{color: "#F9A03E"}}/>
                             </a>
 
@@ -1108,14 +1115,47 @@ function Guia(props) {
         setOpenDialog(false)
     }
 
-    function generarReporteEtiqueta(id, folio) {
-        obtenerGuiaReporteEtiqueta(id).then(({data}) => {
+    function generarReporteEtiqueta(row) {
+        setSeleccionEtiqueta(row)
+        setOpenDialogEtiqueta(true)
+        /*obtenerGuiaReporteEtiqueta(id).then(({data}) => {
             let pdfWindow = window.open("");
             pdfWindow.document.write("<embed  width='100%' height='100%' src='data:application/pdf;base64, " + encodeURI(data) + "'/>");
             pdfWindow.document.body.style.margin = "0px";
             pdfWindow.document.title = "Guía " + folio;
+        })*/
+    }
+    const handleOnChangeReporteEtiqueta = (data) => {
+        console.log(data)
+        setState({
+            ...state,
+            reporteSeleccionado: data
         })
     }
+    const handleGenerarReporteEtiqueta=(e)=>{
+        e.preventDefault()
+        console.log(state.reporteSeleccionado)
+        console.log(seleccion)
+
+        if (state.reporteSeleccionado.length === 0) {
+            showError("Es necesario seleccionar al menos un reporte")
+            return
+        }
+
+        imprimirFormatosIdIdTipoReporte(state.reporteSeleccionado, seleccionEtiqueta.m_nIdGuia).then(({data}) => {
+            console.log(data)
+            let pdfWindow = window.open("");
+            pdfWindow.document.write("<embed  width='100%' height='100%' src='data:application/pdf;base64, " + encodeURI(data.m_sArchivo) + "'/>");
+            pdfWindow.document.body.style.margin = "0px";
+            pdfWindow.document.title = "Guía Etiqueta" + seleccionEtiqueta.m_nFolioGuia;
+        })
+        setState({
+            ...state,
+            reporteSeleccionado: null
+        })
+        setOpenDialogEtiqueta(false)
+    }
+
 
     function generarReporteEtiquetaParcial(params, folio) {
         obtenerGuiaReporteEtiquetaParcial(params).then(({data}) => {
@@ -2094,6 +2134,70 @@ function Guia(props) {
                                                 name="reporteSeleccionado"
                                             >
                                                 {dataReportes.map((reporte) => (
+                                                    <MenuItem
+                                                        key={reporte.m_nIdFormato}
+                                                        value={reporte.m_nIdFormato}
+                                                    >
+                                                        {reporte.m_sFormato}
+                                                    </MenuItem>
+                                                ))}
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>
+                                </Grid>
+                                <DialogActions>
+
+                                    <button className="btn btn-secondary secondary-btn" onClick={() => {
+                                        setOpenDialog(false)
+                                        setState({
+                                            ...state,
+                                            reporteSeleccionado: null
+                                        })
+                                    }
+                                    }>
+                                        Cancelar
+                                    </button>
+                                    <button className="btn btn-primary primary-btn" color={"primary"} type={"submit"}>
+                                        Aceptar
+                                    </button>
+                                </DialogActions>
+                            </form>
+                        </div>
+                    </DialogContent>
+                </Dialog>
+            }
+            {
+                openDialogEtiqueta &&
+                <Dialog
+                    open={openDialogEtiqueta}
+                    onClose={() => setOpenDialogEtiqueta(false)}
+                    fullWidth maxWidth="md"
+                >
+                    <DialogTitle>
+                        Reporte de Guía Etiqueta
+                    </DialogTitle>
+                    <DialogContent>
+                        <div className="row" style={{backgroundColor: '#FFFFFF'}}>
+                            <form onSubmit={handleGenerarReporteEtiqueta}>
+                                <Grid container spacing={1}>
+                                    <Grid item sm={6}>
+                                        <FormControl
+                                            className="input select"
+                                            fullWidth variant="outlined"
+                                            required
+                                            margin="dense">
+                                            <InputLabel
+                                                id="idReporteLabel">Formato de Reporte</InputLabel>
+                                            <Select
+                                                fullWidth
+                                                labelId="idReporteLabel"
+                                                label="Reporte"
+                                                className="form-control"
+                                                value={state.reporteSeleccionado ?? ''}
+                                                onChange={(e) => handleOnChangeReporteEtiqueta(e.target.value)}
+                                                name="reporteSeleccionado"
+                                            >
+                                                {dataReportesEtiqueta.map((reporte) => (
                                                     <MenuItem
                                                         key={reporte.m_nIdFormato}
                                                         value={reporte.m_nIdFormato}
