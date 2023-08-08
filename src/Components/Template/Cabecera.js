d372c63b60630d038288f27a77ff4db5ca55a5d0
Esteban Valencia <evalencia@certuit.com>
2023-08-07 17:08:05 -0700

    aumento de numero de version calidad

--- src/Components/Template/Cabecera.js
+++ src/Components/Template/Cabecera.js
@@ -115,7 +115,7 @@ function Cabecera({ titulo, children }) {
                         
             </div>
             <div>
-                VERSION: 1.6.92
+                VERSION: 1.6.93
             </div>
             <div className="topbar-left pull-left" style={{height: "60px" }}>
                 <div className="clearfix">
