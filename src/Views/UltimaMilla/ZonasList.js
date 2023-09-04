7b9adaa2f806dd1c8c4d63d60bfe9bb8413e24b5
Silvia Elsa Tolosa <stolosa@certuit.com>
2023-09-04 14:56:32 -0700

    Avance mejora

--- src/Views/UltimaMilla/ZonasList.js
+++ src/Views/UltimaMilla/ZonasList.js
@@ -183,6 +183,7 @@ class ZonasList extends Component {
                 <TextField variant="outlined" size={"small"} placeholder={"Buscar"} style={{padding: "0px"}}
                            value={this.state.searchText}
                            onChange={(e) => this.setState({searchText: e.target.value})}
+                           onBlur={()=>this.props.close}
                            InputProps={{
                                endAdornment: (
                                    <InputAdornment position="end">
