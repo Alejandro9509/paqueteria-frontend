aa4cf1eba9f3e9ba489f48d19df92ac00004efca
Esteban Valencia <evalencia@certuit.com>
2023-09-14 09:42:16 -0700

    VERSION: 1.7.13 calidad

--- src/Components/Template/Cabecera.js
+++ src/Components/Template/Cabecera.js
@@ -118,7 +118,7 @@ function Cabecera({ titulo, children }) {
                         
             </div>
             <div>
-                VERSION: 1.7.12
+                VERSION: 1.7.13
             </div>
             <div className="topbar-left pull-left" style={{height: "60px" }}>
                 <div className="clearfix">
