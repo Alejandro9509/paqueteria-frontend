292e95fe23325072173ad4c5621ec651fc71bab7
Alan Ramirez <aramirez@redspira.org>
2023-11-01 17:03:36 -0700

    Se removieron las menciones del paquete rxjs

--- src/Views/Recoleccion.js
+++ src/Views/Recoleccion.js
@@ -114,7 +114,6 @@ import DiferenteDomicilioForm from "./DiferenteDomicilio/DiferenteDomicilioForm"
 import Evidencias from "./Evidencias";
 import DialogoEvidenciasUltimaMilla from "./UltimaMilla/DialogoEvidenciasUltimaMilla";
 import ImportarEmbarques from "./Embarque/ImportarEmbarques";
-import { fromEvent, take } from "rxjs";
 
 let timer;
 
