66e76f51e02814673b3fc17a2501730718af9343
Esteban Valencia <evalencia@certuit.com>
2023-11-14 16:14:32 -0700

    Se dejó reporteador de webdev y se quitó uso de dialogo para seleccionar reporte

--- src/Views/Embarque.js
+++ src/Views/Embarque.js
@@ -258,6 +258,9 @@ const TIPOS_SEGURO = {
     OBLIGATORIO: 4,
     SIN_ASIGNAR: 5
 }
+const FORMATOS_IMPRESION = {
+    EMBARQUE: 211
+}
 function Embarque(props) {
 
 
@@ -283,9 +286,9 @@ function Embarque(props) {
     const [dataRemitenteDestinatario, setDataRemitenteDestinatario,] = React.useState([]);
     const [dataClientes, setDataClientes] = useState([])
     const [stepActive, setStepActive] = React.useState(1);
-    const [openDialog, setOpenDialog] = useState(false)
-    const [dataReportes, setDataReportes] = useState([])
-    const [seleccion, setSeleccion] = useState(null)
+    // const [openDialog, setOpenDialog] = useState(false)
+    // const [dataReportes, setDataReportes] = useState([])
+    // const [seleccion, setSeleccion] = useState(null)
     const [Modal, open, close, isOpen] = useModal("root", {
         preventScroll: true,
     });
@@ -348,7 +351,7 @@ function Embarque(props) {
     const columns = React.useMemo(() => [
         {
             headerName: "Acciones",
-            sortable: false, filterable: false, width: 130,
+            sortable: false, filterable: false, width: 120,
             field: "",
             renderCell: (row) => {
                 return (
@@ -374,14 +377,14 @@ function Embarque(props) {
                                 <i className="fa fa-eye" style={{color: "#F9A03E"}}/>
                             </a>
                         </Tooltip>
-                        <Tooltip title="Reporte opcion 1" disabled={!validarDerecho(9101425)}>
-                            <a className="btn btn-default btn-xs"
-                               onClick={() => generarReporteOpcion1(row.row)}><i
-                                className="zmdi zmdi-file"
-                                style={{color: "#F9A03E"}}/></a>
+                        {/*<Tooltip title="Reporte opcion 1" disabled={!validarDerecho(9101425)}>*/}
+                        {/*    <a className="btn btn-default btn-xs"*/}
+                        {/*       onClick={() => generarReporteOpcion1(row.row)}><i*/}
+                        {/*        className="zmdi zmdi-file"*/}
+                        {/*        style={{color: "#F9A03E"}}/></a>*/}
 
-                        </Tooltip>
-                        <Tooltip title="Reporte opcion 2" disabled={!validarDerecho(9101425)}>
+                        {/*</Tooltip>*/}
+                        <Tooltip title="Reporte" disabled={!validarDerecho(9101425)}>
                             <a className="btn btn-default btn-xs"
                                onClick={() => generarReporte(row.row)}><i
                                 className="zmdi zmdi-file"
@@ -657,12 +660,12 @@ function Embarque(props) {
     });
     let query = useQuery();
 
-    useEffect(()=>{
-
-        obtenerFormatosImpresionProceso(211).then(({data}) => {
-            setDataReportes(data)
-        })
-    }, [])
+    // useEffect(()=>{
+    //
+    //     obtenerFormatosImpresionProceso(211).then(({data}) => {
+    //         setDataReportes(data)
+    //     })
+    // }, [])
 
     //Limpia todos los campos. Se usa al pasar del listado a consultar o modificar un registro
     function limpiarCamposAgregar() {
@@ -933,77 +936,57 @@ function Embarque(props) {
         })
     }
 
-    function generarReporteOpcion1(row) {
-        obtenerEmbarqueReporte(row.m_nIdEmbarque).then(({data}) => {
-            let pdfWindow = window.open("");
-            pdfWindow.document.write("<embed  width='100%' height='100%' src='data:application/pdf;base64, " + encodeURI(data) + "'/>");
-            pdfWindow.document.body.style.margin = "0px";
-            pdfWindow.document.title = "Embarque " + row.m_sFolioEmbarque;
-        })
-    }
+    // function generarReporteOpcion1(row) {
+    //     obtenerEmbarqueReporte(row.m_nIdEmbarque).then(({data}) => {
+    //         let pdfWindow = window.open("");
+    //         pdfWindow.document.write("<embed  width='100%' height='100%' src='data:application/pdf;base64, " + encodeURI(data) + "'/>");
+    //         pdfWindow.document.body.style.margin = "0px";
+    //         pdfWindow.document.title = "Embarque " + row.m_sFolioEmbarque;
+    //     })
+    // }
 
     function generarReporte(row) {
-        setSeleccion(row)
-        console.log(row)
-        setOpenDialog(true)
-      /*  e.preventDefault()
-        console.log(state.reporteSeleccionado)
-        console.log(seleccion)
-
-        if (state.reporteSeleccionado.length === 0) {
-            showError("Es necesario seleccionar al menos un reporte")
-            return
-        }
-
-        imprimirFormatosId(state.reporteSeleccionado, seleccion.IdCita).then(({data}) => { //poner aqui el id de Embarque
-            console.log(data)
-            let pdfWindow = window.open("");
-            pdfWindow.document.write("<embed  width='100%' height='100%' src='data:application/pdf;base64, " + encodeURI(data.m_sArchivo) + "'/>");
-            pdfWindow.document.body.style.margin = "0px";
-            pdfWindow.document.title = "Cita " + seleccion.FolioCita;
+        // setSeleccion(row)
+        // console.log(row)
+        // setOpenDialog(true)
+        obtenerFormatosImpresionProceso(FORMATOS_IMPRESION.EMBARQUE).then((respuesta) => {
+            imprimirFormatosIdIdTipoReporte(respuesta.data[respuesta.data.length - 1]?.m_nIdFormato, row.m_nIdEmbarque).then(({data}) => { //poner aqui el id de Embarque
+                let pdfWindow = window.open("");
+                pdfWindow.document.write("<embed  width='100%' height='100%' src='data:application/pdf;base64, " + encodeURI(data.m_sArchivo) + "'/>");
+                pdfWindow.document.body.style.margin = "0px";
+                pdfWindow.document.title = "Embarque" + row.m_sFolioEmbarque;
+            })
         })
-        setState({
-            ...state,
-            reporteSeleccionado: null
-        })
-        setOpenDialog(false)*/
-       /* obtenerEmbarqueReporte(id).then(({data}) => {
-            let pdfWindow = window.open("");
-            pdfWindow.document.write("<embed  width='100%' height='100%' src='data:application/pdf;base64, " + encodeURI(data) + "'/>");
-            pdfWindow.document.body.style.margin = "0px";
-            pdfWindow.document.title = "Embarque " + folio;
-        })*/
+
     }
-    const handleOnChangeReporte = (data) => {
-        console.log(data)
-        setState({
-            ...state,
-            reporteSeleccionado: data
-        })
-    }
-    const handleGenerarReporte=(e)=>{
-        e.preventDefault()
-        console.log(state.reporteSeleccionado)
-        console.log(seleccion)
-
-        if (state.reporteSeleccionado.length === 0) {
-            showError("Es necesario seleccionar al menos un reporte")
-            return
-        }
-
-        imprimirFormatosIdIdTipoReporte(state.reporteSeleccionado, seleccion.m_nIdEmbarque).then(({data}) => { //poner aqui el id de Embarque
-            console.log(data)
-            let pdfWindow = window.open("");
-            pdfWindow.document.write("<embed  width='100%' height='100%' src='data:application/pdf;base64, " + encodeURI(data.m_sArchivo) + "'/>");
-            pdfWindow.document.body.style.margin = "0px";
-            pdfWindow.document.title = "Embarque" + seleccion.m_sFolioEmbarque;
-        })
-        setState({
-            ...state,
-            reporteSeleccionado: null
-        })
-        setOpenDialog(false)
-    }
+    // const handleOnChangeReporte = (data) => {
+    //     console.log(data)
+    //     setState({
+    //         ...state,
+    //         reporteSeleccionado: data
+    //     })
+    // }
+    // const handleGenerarReporte=(e)=>{
+    //     e.preventDefault()
+    //
+    //     if (state.reporteSeleccionado.length === 0) {
+    //         showError("Es necesario seleccionar al menos un reporte")
+    //         return
+    //     }
+    //
+    //     imprimirFormatosIdIdTipoReporte(state.reporteSeleccionado, seleccion.m_nIdEmbarque).then(({data}) => { //poner aqui el id de Embarque
+    //         console.log(data)
+    //         let pdfWindow = window.open("");
+    //         pdfWindow.document.write("<embed  width='100%' height='100%' src='data:application/pdf;base64, " + encodeURI(data.m_sArchivo) + "'/>");
+    //         pdfWindow.document.body.style.margin = "0px";
+    //         pdfWindow.document.title = "Embarque" + seleccion.m_sFolioEmbarque;
+    //     })
+    //     setState({
+    //         ...state,
+    //         reporteSeleccionado: null
+    //     })
+    //     setOpenDialog(false)
+    // }
 
     const handleOnChangeEntregaDD = (newValue) => {
         setRepetirConceptos(true)
@@ -3098,70 +3081,70 @@ function Embarque(props) {
     }
     return (
         <div>
-            {
-                openDialog &&
-                <Dialog
-                    open={openDialog}
-                    onClose={() => setOpenDialog(false)}
-                    fullWidth maxWidth="md"
-                >
-                    <DialogTitle>
-                        Reporte de Embarque
-                    </DialogTitle>
-                    <DialogContent>
-                        <div className="row" style={{backgroundColor: '#FFFFFF'}}>
-                            <form onSubmit={handleGenerarReporte}>
-                                <Grid container spacing={1}>
-                                    <Grid item sm={6}>
-                                        <FormControl
-                                            className="input select"
-                                            fullWidth variant="outlined"
-                                            required
-                                            margin="dense">
-                                            <InputLabel
-                                                id="idReporteLabel">Formato de Reporte</InputLabel>
-                                            <Select
-                                                fullWidth
-                                                labelId="idReporteLabel"
-                                                label="Reporte"
-                                                className="form-control"
-                                                value={state.reporteSeleccionado ?? ''}
-                                                onChange={(e) => handleOnChangeReporte(e.target.value)}
-                                                name="reporteSeleccionado"
-                                            >
-                                                {dataReportes.map((reporte) => (
-                                                    <MenuItem
-                                                        key={reporte.m_nIdFormato}
-                                                        value={reporte.m_nIdFormato}
-                                                    >
-                                                        {reporte.m_sFormato}
-                                                    </MenuItem>
-                                                ))}
-                                            </Select>
-                                        </FormControl>
-                                    </Grid>
-                                </Grid>
-                                <DialogActions>
+            {/*{*/}
+            {/*    openDialog &&*/}
+            {/*    <Dialog*/}
+            {/*        open={openDialog}*/}
+            {/*        onClose={() => setOpenDialog(false)}*/}
+            {/*        fullWidth maxWidth="md"*/}
+            {/*    >*/}
+            {/*        <DialogTitle>*/}
+            {/*            Reporte de Embarque*/}
+            {/*        </DialogTitle>*/}
+            {/*        <DialogContent>*/}
+            {/*            <div className="row" style={{backgroundColor: '#FFFFFF'}}>*/}
+            {/*                <form onSubmit={handleGenerarReporte}>*/}
+            {/*                    <Grid container spacing={1}>*/}
+            {/*                        <Grid item sm={6}>*/}
+            {/*                            <FormControl*/}
+            {/*                                className="input select"*/}
+            {/*                                fullWidth variant="outlined"*/}
+            {/*                                required*/}
+            {/*                                margin="dense">*/}
+            {/*                                <InputLabel*/}
+            {/*                                    id="idReporteLabel">Formato de Reporte</InputLabel>*/}
+            {/*                                <Select*/}
+            {/*                                    fullWidth*/}
+            {/*                                    labelId="idReporteLabel"*/}
+            {/*                                    label="Reporte"*/}
+            {/*                                    className="form-control"*/}
+            {/*                                    value={state.reporteSeleccionado ?? ''}*/}
+            {/*                                    onChange={(e) => handleOnChangeReporte(e.target.value)}*/}
+            {/*                                    name="reporteSeleccionado"*/}
+            {/*                                >*/}
+            {/*                                    {dataReportes.map((reporte) => (*/}
+            {/*                                        <MenuItem*/}
+            {/*                                            key={reporte.m_nIdFormato}*/}
+            {/*                                            value={reporte.m_nIdFormato}*/}
+            {/*                                        >*/}
+            {/*                                            {reporte.m_sFormato}*/}
+            {/*                                        </MenuItem>*/}
+            {/*                                    ))}*/}
+            {/*                                </Select>*/}
+            {/*                            </FormControl>*/}
+            {/*                        </Grid>*/}
+            {/*                    </Grid>*/}
+            {/*                    <DialogActions>*/}
 
-                                    <button className="btn btn-secondary secondary-btn" onClick={() => {
-                                        setOpenDialog(false)
-                                        setState({
-                                            ...state,
-                                            reporteSeleccionado: null
-                                        })
-                                    }
-                                    }>
-                                        Cancelar
-                                    </button>
-                                    <button className="btn btn-primary primary-btn" color={"primary"} type={"submit"}>
-                                        Aceptar
-                                    </button>
-                                </DialogActions>
-                            </form>
-                        </div>
-                    </DialogContent>
-                </Dialog>
-            }
+            {/*                        <button className="btn btn-secondary secondary-btn" onClick={() => {*/}
+            {/*                            setOpenDialog(false)*/}
+            {/*                            setState({*/}
+            {/*                                ...state,*/}
+            {/*                                reporteSeleccionado: null*/}
+            {/*                            })*/}
+            {/*                        }*/}
+            {/*                        }>*/}
+            {/*                            Cancelar*/}
+            {/*                        </button>*/}
+            {/*                        <button className="btn btn-primary primary-btn" color={"primary"} type={"submit"}>*/}
+            {/*                            Aceptar*/}
+            {/*                        </button>*/}
+            {/*                    </DialogActions>*/}
+            {/*                </form>*/}
+            {/*            </div>*/}
+            {/*        </DialogContent>*/}
+            {/*    </Dialog>*/}
+            {/*}*/}
             {
                 state.showConfirmarUbicacion &&
                 <ConfirmarUbicacion confirmarUbicacion={confirmarUbicacion} open={state.showConfirmarUbicacion}
