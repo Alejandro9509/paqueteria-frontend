27c4a26e8f4ce21ab229a6c1b621f2787b37f3ef
Esteban Valencia <evalencia@certuit.com>
2023-12-08 11:09:37 -0700

    Se actualizo rama cubicaje metiendo cambios de develop

--- src/Views/Informes.js
+++ src/Views/Informes.js
@@ -96,15 +96,6 @@ function showSuccess(mensaje) {
         timeout: "8000",
     }).show();
 }
-function showError(mensaje) {
-    new Noty({
-        type: "error",
-        layout: "topCenter",
-        text: mensaje,
-        timeout: "3000",
-    }).show();
-}
-
 const styles = {
     seleccionado: {
         backgroundColor: "#FCC88F",
