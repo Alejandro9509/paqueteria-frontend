39728793f9f5db35cfcb2cecf1399ec3fe827493
Alan Ramirez <aramirez@redspira.org>
2024-02-19 10:03:16 -0800

    Se centró el texto en el panel de evidencias de seguimiento y tracking y se cambió el label de 'Operador que entrega'

--- src/Views/Evidencias.js
+++ src/Views/Evidencias.js
@@ -58,8 +58,9 @@ function Evidencias(props) {
                         :
                         <Grid item md={12}>
                             <div id="divEmbarque">
-                                Recibió: {props.data.receptorGuia} <br/>
-                                Operador que entregó: {props.data.operadorEntrega}
+                                Entregó Guía: {props.data.operadorEntrega} <br/>
+                                Recibió: {props.data.receptorGuia}
+
                                 <Button fullWidth variant="text" color="primary"
                                         onClick={() => handleClickOpenDialogoEvidencia(true)}>
                                     Ver evidencias de entrega
--- src/Views/Seguimiento/InformacionBasica.js
+++ src/Views/Seguimiento/InformacionBasica.js
@@ -48,10 +48,10 @@ class InformacionBasica extends Component {
                     <Typography style={{fontWeight: "bold", display: "flex", alignItems: "center"}}>Valor declarado: <Typography>${data.m_xValorDeclarado}</Typography></Typography>
                 </Grid>
                 <Grid item xs={3}>
-                    <Typography style={{fontWeight: "bold", display: "flex", alignItems: "center"}}>Operador que entregó: <Typography>{data.operadorEntrega?data.operadorEntrega:'No Aplica'}</Typography> </Typography>
+                    <Typography style={{fontWeight: "bold", display: "flex", alignItems: "center"}}>Recibió: <Typography>{data.m_sReceptor}</Typography> </Typography>
                 </Grid>
                 <Grid item xs={3}>
-                    <Typography style={{fontWeight: "bold", display: "flex", alignItems: "center"}}>Recibió: <Typography>{data.m_sReceptor}</Typography> </Typography>
+                    <Typography style={{fontWeight: "bold", display: "flex", alignItems: "center"}}>Entregó Guía: <Typography>{data.operadorEntrega?data.operadorEntrega:'No Aplica'}</Typography> </Typography>
                 </Grid>
                 <Grid item xs={3}>
                 </Grid>
