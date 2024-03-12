3d3f0e79a06b136ef58852f3f574b8bd2497c8f8
Alan Ramirez <aramirez@redspira.org>
2024-03-12 16:00:17 -0700

    Se ajustó el mensaje al fallar la validación de cancelar guía por motivo de viaje de ERP activo

--- src/Views/Guia.js
+++ src/Views/Guia.js
@@ -1123,7 +1123,7 @@ function Guia(props) {
         }
         else{
             if(respuesta.data.folioViajeERP)
-                showSuccess("La guía no puede ser cancelada ya que pertenece al viaje: "+ respuesta.data.folioViajeERP)
+                showSuccess("La guía no puede ser cancelada ya que pertenece al viaje activo "+ respuesta.data.folioViajeERP +". Cancelar viaje en ERP ")
             else
                 showSuccess("La guía no puede ser cancelada ya que esta siendo usada en el informe: "+ respuesta.data.FolioInforme)
         return
