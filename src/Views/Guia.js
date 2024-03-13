33f01efef0c68cfe2b842953802baf5b32e3a1a4
Esteban Valencia <evalencia@certuit.com>
2024-03-13 13:15:05 -0700

    Se descomentaron lineas de impresion directa

--- src/Views/Guia.js
+++ src/Views/Guia.js
@@ -1736,7 +1736,7 @@ function Guia(props) {
                                     let result
                                     try{
 
-                                        //result = await selected_device.send(TICKET_ZEBRA_TEMPLATE_NOT_QR(guia, paquetesConIndex[i], paquetesConIndex[i].index), undefined, errorCallback)
+                                        result = await selected_device.send(TICKET_ZEBRA_TEMPLATE_NOT_QR(guia, paquetesConIndex[i], paquetesConIndex[i].index), undefined, errorCallback)
                                         await new Promise(resolve => setTimeout(resolve, 1000)); // 3 sec
                                         showSuccess('Impresión en curso.')
                                     }catch (e) {
@@ -1759,7 +1759,7 @@ function Guia(props) {
                         // console.log('paquete: ', paquetesConIndex[i])
                         // console.log((paquetesConIndex[i].index+1) + ' de ' + paquetesConIndex[i].rangoFin)
                         // console.log('index: ', paquetesConIndex[i].index)
-                        //result = await selected_device.send(TICKET_ZEBRA_TEMPLATE_NOT_QR(guia, paquetesConIndex[i], paquetesConIndex[i].index), undefined, errorCallback)
+                        result = await selected_device.send(TICKET_ZEBRA_TEMPLATE_NOT_QR(guia, paquetesConIndex[i], paquetesConIndex[i].index), undefined, errorCallback)
                         await new Promise(resolve => setTimeout(resolve, 1000)); // 3 sec
 
                         // showSuccess('Impresión en curso.')
