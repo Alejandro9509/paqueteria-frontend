b38666f037575ba268962f80939b1cbe84334757
Adrian Renteria <arenteria@certuit.com>
2024-04-24 10:53:05 -0700

    Mensaje de error al cancelar guia

--- src/Views/Guia.js
+++ src/Views/Guia.js
@@ -1183,7 +1183,7 @@ function Guia(props) {
         }
         else{
             if(respuesta.data.folioViajeERP)
-                showSuccess("La guía no puede ser cancelada ya que pertenece al viaje activo "+ respuesta.data.folioViajeERP +". Cancelar viaje en ERP ")
+                showSuccess("La guía no puede ser cancelada ya que pertenece al viaje activo "+ respuesta.data.folioViajeERP +". Cancelar viaje en Trafico/Viajes ")
             else
                 showSuccess("La guía no puede ser cancelada ya que esta siendo usada en el informe: "+ respuesta.data.FolioInforme)
         return
