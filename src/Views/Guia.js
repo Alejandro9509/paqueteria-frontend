88114f3b6ac01ba5fa841f9f36c73bfa8903f19c
Alan Ramirez <aramirez@redspira.org>
2024-05-07 10:03:29 -0700

    Se replicaron cambios de rama 1060

--- src/Views/Guia.js
+++ src/Views/Guia.js
@@ -1205,10 +1205,10 @@ function Guia(props) {
         }
         else{
             if(respuesta.data.folioViajeERP)
-                showSuccess("La guía no puede ser cancelada ya que pertenece al viaje activo "+ respuesta.data.folioViajeERP +". Cancelar viaje en Trafico/Viajes ")
+                showSuccess("La guía no puede ser cancelada ya que pertenece al viaje activo "+ respuesta.data.folioViajeERP +". Cancelar viaje en Tráfico/Viajes ")
             else
                 showSuccess("La guía no puede ser cancelada ya que esta siendo usada en el informe: "+ respuesta.data.FolioInforme)
-        return
+            return
         }
         }).catch((err)=>{
             showSuccess(err)
