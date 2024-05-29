7fbcb59be55b78015ffb85858a5df31e6d1226bc
Alan Ramirez <aramirez@redspira.org>
2024-05-29 10:51:45 -0700

    Al importar funciona la selección de clientes y algunos botones se hicieron más grandes

--- src/Views/Embarque/ImportarEmbarques.js
+++ src/Views/Embarque/ImportarEmbarques.js
@@ -199,7 +199,7 @@ function ImportarEmbarques(props) {
         setState(state => {
             return {
                 ...state,
-                cliente: row.data,
+                cliente: row,
                 /*idTipoSeguro: row.data.m_nIdTipoSeguro !== 0 ? row.data.m_nIdTipoSeguro : 5,
                 porcentajeSeguro:  row.data.m_cPorcentajeSeguro,
                 aplicaSeguro: row.data.m_bTieneSeguro,
@@ -458,6 +458,7 @@ function ImportarEmbarques(props) {
                                         <Grid item xs={1}>
                                             <Button fullWidth
                                                     color={"primary"}
+                                                    style={{fontSize:'1em'}}
                                                     variant={"contained"}
                                                     onClick={() => handleOnImportarClick()}
                                                     disabled={!state.cliente || files.length === 0}
@@ -466,6 +467,7 @@ function ImportarEmbarques(props) {
                                         <Grid item xs={2}>
                                             <Button fullWidth
                                                     color={"primary"}
+                                                    style={{fontSize:'1em'}}
                                                     variant={"outlined"}
                                                     onClick={() => handleOnDescargarPlantillaClick()}
                                                     disabled={!state.cliente}
@@ -475,6 +477,7 @@ function ImportarEmbarques(props) {
                                             <Button fullWidth
                                                     color={"primary"}
                                                     variant={"outlined"}
+                                                    style={{fontSize:'1em'}}
                                                     onClick={() => handleOnLimpiarClick()}
                                             >Limpiar</Button>
                                         </Grid>
