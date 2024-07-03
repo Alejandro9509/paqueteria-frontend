87f1561e15aacb6f2c7cae9cbe2259c688dfb493
Jesus Lopez <jlopez@certuit.com>
2024-07-03 13:19:27 -0700

    Ajuste a las unidades disponibles en ultima milla

--- src/Views/UltimaMilla/UnidadesList.js
+++ src/Views/UltimaMilla/UnidadesList.js
@@ -79,7 +79,8 @@ class UnidadesList extends Component {
             guias: this.props.paquetes.filter(p => !p.m_bEsRecoleccion).map(p => ({m_nIdGuia: p.m_nId}))
         }
         obtenerUnidadesUltimaMilla(this.props.sucursalId, params).then(({data}) => {
-            this.setState({unidades: data})
+            const unidadesDisponibles = data.filter(item => item.ocupado === false);
+            this.setState({unidades: unidadesDisponibles})
         })
     }
 
