d30cf060ac4bd32f3d0e5272cb11e0aa6a62b1ff
Adrian Renteria <arenteria@certuit.com>
2024-08-12 11:42:51 -0700

    Error al eliminar informe

--- src/Views/Viajes/AgregarViaje.js
+++ src/Views/Viajes/AgregarViaje.js
@@ -920,32 +920,37 @@ class AgregarViaje extends Component {
 
     }
 
-    cubicarViaje(arrayInformesAsignados){
-        const paquetes = arrayInformesAsignados.reduce((array1, a) => array1.concat(a.m_arrClsProGuia.reduce((array, i) => array.concat(i.m_arrClsDetalle), [])), []);
-        const params = {
-            idRemolque1: this.state.IdRemolque1?.m_nIdUnidad ?? null,
-            idRemolque2: this.state.IdRemolque2?.m_nIdUnidad ?? null,
-            paquetes: paquetes.map(p => ({
-                alto: p.m_xAlto,
-                ancho: p.m_xAncho,
-                largo: p.m_xLargo,
-                peso: p.m_xPeso,
-                cantidad: p.ctd
-            }))
+    cubicarViaje(arrayInformesAsignados, eliminar = false){
+        if (!eliminar) {
+            const paquetes = arrayInformesAsignados.reduce((array1, a) => array1.concat(a.m_arrClsProGuia.reduce((array, i) => array.concat(i.m_arrClsDetalle), [])), []);
+            const params = {
+                idRemolque1: this.state.IdRemolque1?.m_nIdUnidad ?? null,
+                idRemolque2: this.state.IdRemolque2?.m_nIdUnidad ?? null,
+                paquetes: paquetes.map(p => ({
+                    alto: p.m_xAlto,
+                    ancho: p.m_xAncho,
+                    largo: p.m_xLargo,
+                    peso: p.m_xPeso,
+                    cantidad: p.ctd
+                }))
+            }
+            cubicarGuia(params).then(({data}) => {
+                this.setState({utilizacion: data.utilizacion.toFixed(0)})
+            }).catch(e => {
+                this.setState({utilizacion: 0})
+                if (!eliminar) {
+                    showError(e.response?.data)
+                }
+
+            })
         }
-        cubicarGuia(params).then(({data}) => {
-            this.setState({utilizacion: data.utilizacion.toFixed(0)})
-        }).catch(e => {
-            this.setState({utilizacion: 0})
-            showError(e.response?.data)
-        })
     }
 
 
     handleEliminarInforme(id) {
         var dataInformesAsignados = [...this.state.dataInformesAsignados]
         dataInformesAsignados.splice(dataInformesAsignados.findIndex(i => i.m_nIdInforme === id), 1)
-        this.cubicarViaje(dataInformesAsignados)
+        this.cubicarViaje(dataInformesAsignados, true)
         this.setState({dataInformesAsignados: dataInformesAsignados})
     }
 
