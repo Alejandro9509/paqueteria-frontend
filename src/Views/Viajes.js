5f15bc1ab4a5ea4110e1710ddd01df271d70b6cc
Adrian Renteria <arenteria@certuit.com>
2024-08-30 10:58:56 -0700

    No se mostraba el ultimo folio fiscal

--- src/Views/Viajes.js
+++ src/Views/Viajes.js
@@ -1058,7 +1058,7 @@ function Viajes() {
             headerName: "Folio Fiscal sustituido",
             field: "m_nIdOrigen",
             width: 300,
-            valueFormatter: row => row.m_sFolioFiscalUUIDSustituido || row.m_sUltimoFolioFiscalUUIDSustituido || " "
+            valueFormatter: row => row.m_sFolioFiscalUUIDSustituido == "" ?  row.m_sUltimoFolioFiscalUUIDSustituido : (row.m_sFolioFiscalUUIDSustituido || "")
         },
         // {
         //     headerName: "Liq",
