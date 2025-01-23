d0ebb8a697381bf3ec8322dc1d990df3992abbb3
Astrid Angulo <astrid.desarrollo@gmtransporterp.com>
2025-01-22 16:59:05 -0800

    refactor: :zap: Se cambió el url del servicio para obtener estados por id de pais
    Debido a errores en el back de webdev se cambió el servicio para obtener estados por país para obtenerlo desde el back de springboot

--- src/Util/Contexts/EstadosContext.js
+++ src/Util/Contexts/EstadosContext.js
@@ -6,7 +6,7 @@ const headers = API_HEADERS
 
 
 function obtenerEstadosPais(id) {
-    const url = `${process.env.REACT_APP_API_URL}/Estados/ByPais/${id}`;
+    const url = `${process.env.REACT_APP_REPORT_URL}/api/Estados/ByPais/${id}`;
     let result;
     trackPromise(
         result =  axios.get(url, { headers })
