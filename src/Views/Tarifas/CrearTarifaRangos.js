f5011fa4e51c6628a0c38c6f18c3e5b7e1234fab
Astrid Angulo <astrid.desarrollo@gmtransporterp.com>
2025-01-27 15:33:17 -0800

    fix: :ambulance: Corrección de import faltante
    Se volvió a agregar un import faltante en CrearTarifasRangos

--- src/Views/Tarifas/CrearTarifaRangos.js
+++ src/Views/Tarifas/CrearTarifaRangos.js
@@ -2,7 +2,7 @@ import React, {useEffect, useMemo, useState} from 'react';
 import {
     Button,
     createFilterOptions,
-    Dialog,
+    Dialog, DialogActions,
     DialogContent,
     DialogTitle,
     FormControl,
