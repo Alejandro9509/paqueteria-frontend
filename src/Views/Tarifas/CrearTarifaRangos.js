54a0f2d6718ebbb1ecde8fd51794cbce355a2604
Astrid Angulo <astrid.desarrollo@gmtransporterp.com>
2025-01-28 17:27:05 -0800

    fix: :bug: Validaciones en formulario de viajes de milla intermedia para grupos repetidos
    En el diálogo de los viajes de milla intermedia se agregó una validación faltante para que cuando los grupos de este viaje tengan una relación zonas-productos repetida se indique el error

--- src/Views/Tarifas/CrearTarifaRangos.js
+++ src/Views/Tarifas/CrearTarifaRangos.js
@@ -318,20 +318,31 @@ export default function CrearTarifaRangos(props) {
                 i.grupos = viajeForaneo.grupos
             }
         })
-        // console.log(viajeForaneo)
-        // console.log(viajesForaneosListado)
+        console.log(viajeForaneo)
+        console.log(viajesForaneosListado)
         let duplicado = false;
+        let duplicadoInterno = false;
         let zonasRepetidas = "Zonas duplicadas: ";
         let productosDuplicados = "Productos duplicados: ";
+        let gruposRepetidos = "";
+        /*
+        * Revisamos si en el listado de viajes de la tarifa hay alguno con el mismo origen-destino (sin tomar en cuenta
+        * el viaje que estamos modificando) y checamos si en sus zonas y productos hay coincidencias con el viaje
+        * que estamos modificando
+        * */
         viajesForaneosListado.forEach(viaje => {
             if(viaje.idOrigen === viajeForaneo.idOrigen && viaje.idDestino == viajeForaneo.idDestino && viajeForaneo.idViaje != viaje.idViaje){
                 viajeForaneo.grupos.forEach(grupoViajeNuevo => {
                     const zonasNuevas = grupoViajeNuevo.zonas.map(i => (i.m_nIdZona))
                     const productosNuevos = grupoViajeNuevo.productos.map(i => (i.m_nIdProducto))
+                    // console.log(zonasNuevas);
+                    // console.log(productosNuevos);
 
                     viaje.grupos.forEach(grupoViaje => {
                         const zonas = grupoViaje.zonas.map(i => (i.m_nIdZona))
                         const productos = grupoViaje.productos.map(i => (i.m_nIdProducto))
+                        // console.log(zonas);
+                        // console.log(productos);
 
                         if(zonasNuevas.some(z => zonas.includes(z)) && productosNuevos.some(p => productos.includes(p))){
                             duplicado = true;
@@ -350,15 +361,47 @@ export default function CrearTarifaRangos(props) {
                 })
             }
         })
+        /*
+        * Ahora después de revisar la lista de viajes, debemos verificar si en los grupos del viaje que modificamos hay
+        * repeticiones
+        * */
+        let gruposBusqueda = viajeForaneo.grupos.map(i => (i));
+        viajeForaneo.grupos.forEach(grupo => {
+            const zonas = grupo.zonas.map(i => (i.m_nIdZona))
+            const productos = grupo.productos.map(i => (i.m_nIdProducto))
+            console.log("Comparamos desde " + grupo.nombre)
+            console.log(zonas);
+            console.log(productos);
+            //console.log(listadoFiltrado)
+            gruposBusqueda.filter(i => (i.idGrupo !== grupo.idGrupo)).forEach(g => {
+                console.log("Con el grupo " + g.nombre);
+                const z = g.zonas.map(i => (i.m_nIdZona))
+                const p = g.productos.map(i => (i.m_nIdProducto))
+                console.log(z);
+                console.log(p);
+                if(z.some(zona => zonas.includes(zona)) && p.some(prod => productos.includes(prod))){
+                    duplicadoInterno = true;
+                    gruposRepetidos += g.nombre + "-" + grupo.nombre + ", ";
+                }
+            })
+            gruposBusqueda = gruposBusqueda.filter(j => (j.idGrupo !== grupo.idGrupo));
+        })
+
         zonasRepetidas = zonasRepetidas.substring(0, zonasRepetidas.length - 2);
         productosDuplicados = productosDuplicados.substring(0, productosDuplicados.length - 2);
         if(duplicado){
-            showSuccess("En el listado de viajes ya existe un viaje con la misma relación de origen-destino, " +
-                "zonas y productos. Favor de revisar que la información no se repita. " + zonasRepetidas + ". " + productosDuplicados);
-        }else {
+            showSuccess("En el listado de viajes ya existe un viaje con un grupo con la misma relación de " +
+                "origen-destino, zonas y productos. Favor de revisar que la información no se repita. "
+                + zonasRepetidas + ". " + productosDuplicados);
+        }
+        if(duplicadoInterno){
+            showSuccess("Dentro de este mismo viaje hay grupos que tienen la misma relación zona-producto. " +
+                "Favor de revisar los grupos: " + gruposRepetidos);
+        }
+        if(!duplicado && !duplicadoInterno) {
             showSuccess("Guardado");
-            setViajesForaneosListado(newViajes);
-            setOpenForaneo(false);
+            // setViajesForaneosListado(newViajes);
+            // setOpenForaneo(false);
         }
     }
 
